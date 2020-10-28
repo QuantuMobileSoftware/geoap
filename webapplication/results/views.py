2ec0462946b2bfebf43f4d860c807de54393555c
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2020-10-28 16:54:48 +0200

    fix webapplication/results/views.py

--- webapplication/results/views.py
+++ webapplication/results/views.py
@@ -1,32 +1,27 @@
-# from django.shortcuts import render
-from django.views import View
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from django.http import HttpResponse
-from django.conf import settings
 
 
-class FilesView(View):
+class FilesView(APIView):
     """
     Get files from local storage.
     """
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request, *args, **kwargs) -> HttpResponse:
         """
         The parts are then rebuilt by NGINX and used with proxy_pass.
         """
-        if request.user and request.user.is_authenticated:
-            print('user is auth')
-            file_path = kwargs['file_path']
-            file_name = kwargs['file_name']
-            file = f'/file_download/{file_path}/{file_name}'
-            print('dyman', file)
-            response = HttpResponse()
-            response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_name)
-            response['X-Accel-Redirect'] = file
-            del response['Content-Type']
-            del response['Accept-Ranges']
-            del response['Set-Cookie']
-            del response['Cache-Control']
-            del response['Expires']
-            return response
-
-        print('hohoho')
-        return HttpResponse(status=400)
+        file_path = kwargs['file_path']
+        file_name = kwargs['file_name']
+        file = f'/file_download/{file_path}/{file_name}'
+        response = HttpResponse()
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_name)
+        response['X-Accel-Redirect'] = file
+        del response['Content-Type']
+        del response['Accept-Ranges']
+        del response['Set-Cookie']
+        del response['Cache-Control']
+        del response['Expires']
+        return response
