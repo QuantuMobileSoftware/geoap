cc92b37b51bd9e340cba15965eba6109acc7aa0e
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2020-10-30 12:16:33 +0200

    fix webapplication/results/urls.py, rm old db settings from webapplication/sip/settings.py

--- webapplication/results/urls.py
+++ webapplication/results/urls.py
@@ -5,5 +5,5 @@ app_name = "results"
 
 
 urlpatterns = [
-    path('files/<file_path>/<file_name>', FilesView.as_view(), name='get_file'),
+    path('files/<path:file_path>/<file_name>', FilesView.as_view(), name='get_file'),
 ]
--- webapplication/sip/settings.py
+++ webapplication/sip/settings.py
@@ -102,12 +102,6 @@ WSGI_APPLICATION = 'sip.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('POSTGRES_DB', ''),
         'USER': os.getenv('POSTGRES_USER', ''),
