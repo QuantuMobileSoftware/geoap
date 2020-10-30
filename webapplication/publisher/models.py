63fe61a5394410be7632c50466eaee7b8570ad10
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-10-30 17:05:11 +0200

    13504: add db index, dates default

--- webapplication/publisher/models.py
+++ webapplication/publisher/models.py
@@ -1,10 +1,10 @@
 from django.contrib.gis.db import models
 from django.contrib.postgres.fields import JSONField
+from datetime import date
 
 
-# Create your models here.
 class Results(models.Model):
-    filename = models.CharField(max_length=200, unique=True, verbose_name='Original filename')
+    filename = models.CharField(max_length=200, unique=True, db_index=True, verbose_name='Original filename')
     modified = models.DateTimeField(verbose_name='File modification date')
     abs_url = models.URLField(max_length=400, verbose_name='File absolute URL')
 
@@ -33,6 +33,7 @@ class Results(models.Model):
     description = models.TextField(null=True, verbose_name='Layer description')
     released = models.BooleanField(default=False, verbose_name='Released')
 
-    start = models.DateField(verbose_name='Start date')
-    end = models.DateField(verbose_name='End date')
-    name = models.CharField(max_length=200, verbose_name='Layer name')
+    start_date = models.DateField(verbose_name='Start date', default=date.today)
+    end_date = models.DateField(verbose_name='End date', default=date.today)
+    name = models.CharField(max_length=200, null=True, verbose_name='Layer name')
+
