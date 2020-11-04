767c7cc2065e2d2a297fdeb82d483477d0cd68f9
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-04 16:09:02 +0200

    13530 and 13531: add admin to publisher

--- webapplication/publisher/admin.py
+++ webapplication/publisher/admin.py
@@ -1,3 +1,35 @@
-from django.contrib import admin
+from django.contrib.gis import admin
+from .models import Result
+# from django.contrib.postgres import fields
+from django.db.models import JSONField
+from flat_json_widget.widgets import FlatJsonWidget
 
-# Register your models here.
+@admin.register(Result)
+class ResultAdmin(admin.OSMGeoAdmin):
+
+    list_display = ('filepath', 'layer_type', 'modifiedat', 'start_date', 'end_date', 'released', )
+    list_filter = ('layer_type', 'start_date', 'end_date', 'released', )
+    search_fields = ('filepath', 'description', 'options', )
+    readonly_fields = ('filepath', 'layer_type', 'modifiedat', 'rel_url', )
+
+    fieldsets = (
+        ('Fill by Publisher', {
+            'fields': ('filepath', 'layer_type', 'modifiedat', 'rel_url', 'polygon', )
+        }),
+        ('Fill by Data Scientist', {
+            'fields': ('name', 'options', 'description', 'start_date', 'end_date', 'released', )
+        }),
+    )
+
+    formfield_overrides = {
+        JSONField: {
+            'widget': FlatJsonWidget,
+        },
+    }
+
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
