b8247960d837433eefe6d79addec27512ee2e9be
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-04 16:11:00 +0200

    13530 and 13531: add xyz tiles generation

--- webapplication/publisher/management/commands/publish.py
+++ webapplication/publisher/management/commands/publish.py
@@ -2,8 +2,11 @@ import logging
 import fcntl
 import os
 import os.path
+import shutil
 import tempfile
+import time
 
+from subprocess import Popen, PIPE, TimeoutExpired
 from django.core.management.base import BaseCommand
 from ._File import Geojson, Geotif
 from ...models import Result
@@ -28,6 +31,17 @@ def instance_already_running(label='default'):
 
     return already_running
 
+def rm_empty_dirs(folder):
+    """
+       Remove empty dirs and sub dirs
+    """
+    for dirpath, _, _, in os.walk(folder, topdown=False):
+        if dirpath != folder:
+            try:
+                os.rmdir(dirpath)
+            except OSError:
+                pass
+
 
 class Command(BaseCommand):
     help = 'Publisher polls results folder every 60 sec and updates results table'
@@ -41,7 +55,7 @@ class Command(BaseCommand):
         parser.add_argument(
             '-p',
             '--path',
-            default="./data/results",
+            default='./data/results',
             type=str,
             help='Path to results folder, default: ./data/results'
         )
@@ -78,11 +92,11 @@ class Command(BaseCommand):
                     result = Result.objects.get(filepath=file_dict['filepath'])
                     if result.modifiedat < file_dict['modifiedat']:
                         Result.objects.filter(id=result.id).update(**file_dict)
-                        # TODO(adolgarev) generate tiles
+                        self.to_xyz(file)
                         logger.info(f"Object {file_dict['filepath']} was UPDATED")
                 except Result.DoesNotExist:
                     Result.objects.create(**file_dict)
-                    # TODO(adolgarev) generate tiles
+                    self.to_xyz(file)
                     logger.info(f"Object {file_dict['filepath']} was CREATED")
             except Exception as ex:
                 logger.error(f"Error for {file_dict['filepath']}: {str(ex)}")
@@ -91,11 +105,59 @@ class Command(BaseCommand):
     def _clean(self, files):
         filepaths = [file.filepath() for file in files]
         to_delete = Result.objects.exclude(filepath__in=filepaths)
-        logger.info(f"Deleting {len(to_delete)} objects...\FILEPATHS: "
+        xyz_delete = to_delete.filter(layer_type=Result.XYZ)
+
+        logger.info(f"Deleting {to_delete.count()} objects. FILEPATHS: "
                           f"{[file.filepath for file in to_delete]}")
         try:
+            self._delete_xyz_tile(xyz_delete)
             to_delete.delete()
         except Exception as ex:
             logger.error(f"Error deleting: {str(ex)}")
-        # TODO(adolgarev) remove tiles
         logger.info(f"Deleting finished")
+
+    def to_xyz(self, file, timeout=60 * 5, sleep=5,
+               folder="./data/tiles/"):
+        if file.layer_type() != Result.XYZ:
+            return
+
+        abs_path = file.path
+        save_path = f"{folder}{file.filepath().split('.')[0]}"
+        logger.info(f"Generating xyz tile for {abs_path}")
+
+        command = ["gdal2tiles.py", "--xyz", "--webviewer=none", "--zoom=10-16", abs_path, save_path, ]
+
+        process = Popen(command, stdout=PIPE)
+        try:
+            out, err = process.communicate(timeout=timeout)
+            logger.info(f"Process output:\n{out.decode('utf-8')}\nProcess err: {err}")
+            self._poll_process(process, sleep)
+        except TimeoutExpired as te:
+            logger.error(f"Process error: {str(te)}. Killing process...")
+            process.kill()
+            self._poll_process(process, sleep)
+
+    def _poll_process(self, process, sleep):
+        while True:
+            code = process.poll()
+            if code == 0 or code:
+                logger.info(f"Process was finished with code: {code}")
+                break
+            else:
+                logger.info(f"Process is running...")
+                time.sleep(sleep)
+
+    def _delete_xyz_tile(self, xyz_delete, folder="./data/tiles/"):
+        logger.info(f"Deleting {xyz_delete.count()} xyz tiles...")
+        for item in xyz_delete:
+            item_dir = item.filepath.split('.')[0]
+            delete_dir = f"{folder}{item_dir}"
+            logger.info(f"Deleting xyz by path: {delete_dir}")
+            try:
+                shutil.rmtree(delete_dir)
+                rm_empty_dirs(folder)
+            except OSError as e:
+                logger.error(f"Error: {delete_dir}: {e.strerror}")
+        logger.info("Deleting xyz tiles finished")
+
+
