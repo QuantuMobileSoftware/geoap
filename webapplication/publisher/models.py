eb68a07e90cd7cb76515190caf06bd3499b39044
Vladyslav Yaloveha <42910560+vladyslav1997@users.noreply.github.com>
2020-11-05 20:45:33 +0200

    13530 13531 vy admin geotif (#6)
    * 13530 and 13531: upd migration
    
    * 13530 and 13531: small fixes
    
    * 13530 and 13531: add admin to publisher
    
    * 13530 and 13531: upd model for django api
    
    * 13530 and 13531: add xyz tiles generation
    
    * 13530 and 13531: update to django 3.1
    
    * 13530 and 13531: add json widget, upd datetime format
    
    * 13530 and 13531: replace quotes
    
    * 13530 and 13531: init migration
    
    * 13530: add generate, delete tile; use os.path, revert filename
    
    * 13530: rm commented line
    
    * 13530: rm srid
    
    * 13530: upd tile generation and delete, rm process poll
    
    * 13530: add TILES_FOLDER, RESULTS_FOLDER
    
    * 13530: revert init migration
    
    * 13530: change zoom to 10-16
    
    * 13530: Add FileFactory, add missing migrations
    
    Co-authored-by: Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
    Co-authored-by: adolgarev <a.dolgarev@quantumobile.com>

--- webapplication/publisher/models.py
+++ webapplication/publisher/models.py
@@ -1,5 +1,5 @@
 from django.contrib.gis.db import models
-from django.contrib.postgres.fields import JSONField
+from django.db.models import JSONField
 from datetime import date
 
 
@@ -7,29 +7,35 @@ class Result(models.Model):
     filepath = models.CharField(max_length=200, unique=True, db_index=True, verbose_name='Original filepath')
     modifiedat = models.DateTimeField(verbose_name='Original file modification date')
 
-    STRING = 'STRING'
     GEOJSON = 'GEOJSON'
     XYZ = 'XYZ'
 
     LAYER_TYPE_CHOICES = [
-        (STRING, 'STRING'),
         (GEOJSON, 'GEOJSON'),
         (XYZ, 'XYZ'),
     ]
     layer_type = models.CharField(
         max_length=7,
         choices=LAYER_TYPE_CHOICES,
-        verbose_name='Layer type'
+        verbose_name='Layer type',
     )
 
     polygon = models.PolygonField(spatial_index=True, verbose_name='Polygon')
     rel_url = models.URLField(max_length=400, verbose_name='Layer URL')
 
     # Filled in by a Data science engineer
-    options = JSONField(null=True, verbose_name='Layer options')
-    description = models.TextField(null=True, verbose_name='Layer description')
+    options = JSONField(blank=True, null=True, verbose_name='Layer options')
+    description = models.TextField(blank=True, null=True, verbose_name='Layer description')
     released = models.BooleanField(default=False, verbose_name='Released')
 
     start_date = models.DateField(verbose_name='Start date', default=date.today)
     end_date = models.DateField(verbose_name='End date', default=date.today)
-    name = models.CharField(max_length=200, null=True, verbose_name='Layer name')
+    name = models.CharField(max_length=200, blank=True, null=True, verbose_name='Layer name')
+
+    def __str__(self):
+        return self.filepath
+
+    class Meta:
+        verbose_name = 'Result'
+        verbose_name_plural = 'Results'
+        ordering = ['-modifiedat']
