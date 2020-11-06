a65a2c38ca81d4a476610697a7ef2ad76d04624d
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-06 16:20:54 +0200

    13621_13622: rm start end date default values

--- webapplication/publisher/migrations/0002_auto_20201105_1843.py
+++ webapplication/publisher/migrations/0002_auto_20201105_1843.py
@@ -34,14 +34,4 @@ class Migration(migrations.Migration):
             name='options',
             field=models.JSONField(blank=True, null=True, verbose_name='Layer options'),
         ),
-        migrations.AlterField(
-            model_name='result',
-            name='end_date',
-            field=models.DateField(blank=True, null=True, verbose_name='End date'),
-        ),
-        migrations.AlterField(
-            model_name='result',
-            name='start_date',
-            field=models.DateField(blank=True, null=True, verbose_name='Start date'),
-        ),
     ]
