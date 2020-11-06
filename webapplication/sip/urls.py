e0026ff372bef71040c5ed7e3f1091e006674dc7
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-11-06 12:46:15 +0200

    13621_13622: add swagger to urls

--- webapplication/sip/urls.py
+++ webapplication/sip/urls.py
@@ -1,23 +1,23 @@
 """
 SIP URL Configuration
 """
+
 from django.contrib import admin
 from django.urls import path, include
-
+from .docs_drf_yasg import urlpatterns as doc_urls
 from rest_auth.views import LoginView, LogoutView, UserDetailsView
-from rest_framework_swagger.views import get_swagger_view
 
-schema_view = get_swagger_view(title='SIP APIs')
 
 api_patterns = [
     path('login', LoginView.as_view(), name='rest_login'),
     path('logout', LogoutView.as_view(), name='rest_logout'),
     path('users/current', UserDetailsView.as_view(), name='rest_user_details'),
-    path('docs', schema_view),
     path('', include("publisher.urls")),
 ]
+api_patterns.extend(doc_urls)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(api_patterns)),
 ]
+
