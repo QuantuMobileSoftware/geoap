c5cbab4a8651ec42110216ce2ae2f51a1325b210
adolgarev <a.dolgarev@quantumobile.com>
2020-11-06 18:34:48 +0200

    Prod settings updated

--- webapplication/sip/settings-prod.py
+++ webapplication/sip/settings-prod.py
@@ -2,4 +2,8 @@ from sip.settings import *
 
 DEBUG = False
 
-# ALLOWED_HOSTS.append("<REPLACE WITH DOMAIN NAME>")
+ALLOWED_HOSTS.append("192.168.1.61")
+
+# Publisher app
+TILES_FOLDER = "/data/tiles"
+RESULTS_FOLDER = "/data/results"
