e1b46ec910623e745c684a727d61f511157071fa
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2020-11-07 14:32:05 +0200

    13541_crud_for_results done

--- webapplication/publisher/urls.py
+++ webapplication/publisher/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import FilesView
+from .views import FilesView, ResultListAPIView, ResultRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path('files/<path:file_path>', FilesView.as_view(), name='get_file'),
+    path('results', ResultListAPIView.as_view(), name='get_results'),
+    path('results/<int:pk>', ResultRetrieveUpdateDestroyAPIView.as_view(), name='rud_result')
 ]
