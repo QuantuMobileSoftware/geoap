fe059c23d31d4050ff58b1e2b3e87137a19ad80d
DmitriyLugovoy <62790836+DmitriyLugovoy@users.noreply.github.com>
2020-11-10 18:48:25 +0200

    13540 crud for aoi (#11)
    * 13541_crud_for_results done
    
    * 13541_crud_for_results done
    
    * 13541 code to pep8
    
    * 13541 add rm result files
    
    * 13540_CRUD_for_AOI done
    
    * fix for webapplication/publisher/management/commands/publish.py
    
    * add fix for area_of_interest/views.py
    
    * 13540 mv app to publisher
    
    * 13540: rename create_date to createdat, remove description, filter out to_be_deleted
    
    Co-authored-by: adolgarev <a.dolgarev@quantumobile.com>

--- webapplication/publisher/serializers.py
+++ webapplication/publisher/serializers.py
@@ -2,7 +2,8 @@
 Results serializer module.
 """
 from rest_framework import serializers
-from .models import Result
+from .models import Result, AoI
+
 
 class ResultSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,15 @@ class ResultSerializer(serializers.ModelSerializer):
         fields = ('id', 'filepath', 'modifiedat', 'layer_type', 'polygon', 'rel_url', 'options', 'description',
                   'released', 'start_date', 'end_date', 'name', 'to_be_deleted')
         read_only_fields = ['filepath', 'fmodifiedat', 'layer_type', 'polygon', 'rel_url', 'to_be_deleted']
+
+
+class AoISerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        validated_data.pop('createdat', None)
+        return AoI.objects.create(**validated_data)
+
+    class Meta:
+        model = AoI
+        fields = ('id', 'name', 'polygon', 'createdat')
+        read_only_fields = ['createdat', ]
