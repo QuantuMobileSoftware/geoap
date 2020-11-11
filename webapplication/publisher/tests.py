c544c62da2d241d0a12e4222c5de5705dc63ba7b
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2020-11-11 21:14:42 +0200

    13681 create base test

--- webapplication/publisher/tests.py
+++ webapplication/publisher/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from user.models import User
 
-# Create your tests here.
+
+class BaseTestCase(TestCase):
+    """
+    Base test case.
+    """
+
+    def setUp(self):
+        self.staff = self.create_user(username='test_staff_user', is_staff=True)
+        self.not_staff_user = self.create_user(username='test_user')
+
+    def create_user(self, username, is_staff=False, is_superuser=False):
+        """
+        create_user
+        """
+        user = User.objects.create(username=username, is_staff=is_staff, is_superuser=is_superuser)
+        return user
