c4cacd3e83c0df22b0b5211cdee74b7eec73b484
Aleksandr Dolgaryev <adolgarev@gmail.com>
2020-11-15 18:13:08 +0200

    13713 Login screen and mapbox map (#16)
    * 13713: login screen
    
    * 13713: style login form and add alert on error
    
    * 13713: add leaflet with a default mapbox basemap
    
    Co-authored-by: adolgarev <a.dolgarev@quantumobile.com>

---
+++ webviewer/src/LoginForm.js
@@ -0,0 +1,22 @@
+"use strict";
+
+import { Div } from "@adolgarev/domwrapper/src";
+
+export default function createLoginForm(widgetFactory, userModel) {
+    const usernameField = widgetFactory.createTextField("Username");
+    const passwordField = widgetFactory.createPasswordField("Password");
+    const loginButton = widgetFactory.createButton("Login").addEventListener("click", () => {
+        userModel.login(usernameField.getValue(), passwordField.getValue());
+    });
+    const box = Div().setStyle({
+        position: "absolute",
+        top: "0",
+        bottom: "0",
+        left: "0",
+        right: "0",
+        margin: "auto",
+        height: "300px",
+        width: "19em"
+    }).setChildren(usernameField, passwordField, loginButton);
+    return box;
+}
--- webviewer/webpack.config.js
+++ webviewer/webpack.config.js
@@ -14,7 +14,22 @@ module.exports = {
             template: require("html-webpack-template"),
             title: "Webviewer • SIP",
             hash: true,
-            mobile: true
+            mobile: true,
+            links: [
+                {
+                    href: "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css",
+                    rel: "stylesheet",
+                    integrity: "sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==",
+                    crossorigin: ""
+                }
+            ],
+            scripts: [
+                {
+                    src: "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js",
+                    integrity: "sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==",
+                    crossorigin: ""
+                }
+            ]
         })
     ],
 
