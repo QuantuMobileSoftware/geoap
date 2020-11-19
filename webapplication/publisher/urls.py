f9cbc7b82e5f677db379729986e831f23cb1bfc5
DmitriyLugovoy <62790836+DmitriyLugovoy@users.noreply.github.com>
2020-11-19 18:07:46 +0200

    13681 unit tests for results endpoints (#14)
    * 13681 create base test
    
    * 13681 update settings
    
    * 13681 add data to test_db.results
    
    * 13681 add test.py to test_db.results
    
    * 13681 fixes
    
    * 13681: do not map local folder for tests as data
    
    * 13681 add new tests, fix old
    
    * add tests for aoi
    
    * 13681 fix rel_url creating hardcode
    
    * 13681 add tests for publiser
    
    * 13681 add publisher tests
    
    * 13681 publisher tests done
    
    * 13681 all results, aoi, publisher tests are done
    
    * 13681: remove volume, wait for db
    
    * 13681 fixes
    
    Co-authored-by: adolgarev <a.dolgarev@quantumobile.com>

--- webapplication/publisher/urls.py
+++ webapplication/publisher/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('files/<path:file_path>', FilesView.as_view(), name='get_file'),
     path('results', ResultListAPIView.as_view(), name='get_results'),
     path('results/<int:pk>', ResultRetrieveUpdateDestroyAPIView.as_view(), name='result'),
-    path('aoi', AoIListCreateAPIView.as_view(), name='aoi'),
-    path('aoi/<int:pk>', AoIRetrieveUpdateDestroyAPIView.as_view(), name='rud_aoi'),
+    path('aoi', AoIListCreateAPIView.as_view(), name='aoi_list_or_create'),
+    path('aoi/<int:pk>', AoIRetrieveUpdateDestroyAPIView.as_view(), name='aoi'),
     path('aoi/<int:pk>/results', AOIResultsListAPIView.as_view(), name='aoi_results'),
 ]
