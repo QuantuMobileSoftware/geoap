e0a6b7cce2ae609ce6c8929f4aea56900cfccf6c
Aleksandr Dolgaryev <adolgarev@gmail.com>
2020-11-22 15:54:14 +0200

    13753 layers (#20)
    * 13753: add error message bar
    
    * 13753: add layers selector container
    
    * 13753: show selected layer
    
    Co-authored-by: adolgarev <a.dolgarev@quantumobile.com>

--- webviewer/src/Map.js
+++ webviewer/src/Map.js
@@ -4,24 +4,66 @@ import { Div } from "@adolgarev/domwrapper/src";
 
 export default function createMap(widgetFactory, mapModel) {
     const mapId = widgetFactory.generateRandomId("Map");
-    const map = Div()
+    const mapElt = Div()
         .setAttribute("id", mapId)
         .setStyle({
             width: "100%",
             height: "100%",
             position: "absolute",
             top: "0",
-            left: "0"
+            left: "0",
+            "z-index": "1",
         });
-    map.componentDidMount = () => {
-        const mymap = L.map(mapId);
-        mymap.fitBounds([[49.8704624780525, 37.0123590916912], [50.040247115589196, 36.737865323126336]]);
-        L.tileLayer("/tiles/mapbox/{z}/{x}/{y}.png", {
-            attribution: "Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-            maxZoom: 18,
-            tileSize: 512,
-            zoomOffset: -1
-        }).addTo(mymap);
+    var map = null;
+    mapElt.componentDidMount = () => {
+        map = L.map(mapId, { zoomControl: false });
+        if (process.env.NODE_ENV === "development") {
+            L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+                attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors",
+                maxZoom: 16
+            }).addTo(map);
+        } else {
+            L.tileLayer("/tiles/mapbox/{z}/{x}/{y}.png", {
+                attribution: "Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+                maxZoom: 16,
+                tileSize: 512,
+                zoomOffset: -1
+            }).addTo(map);
+        }
     };
-    return map;
+    var layer = null;
+    mapModel.addEventListener("layerselected", () => {
+        if (map === null) {
+            return;
+        }
+        if (layer !== null) {
+            layer.remove();
+        }
+        const l = mapModel.selectedLayer;
+        if (l.layer_type === "GEOJSON") {
+            layer = L.geoJSON();
+            const xhr = new XMLHttpRequest();
+            xhr.open("GET", l.rel_url);
+            xhr.onload = () => {
+                if (xhr.status !== 200) {
+                    console.error(xhr.responseText);
+                } else {
+                    const jsonResponse = JSON.parse(xhr.responseText);
+                    layer.addData(jsonResponse);
+                }
+            };
+            xhr.onerror = () => {
+                console.error("Failed to request " + l.rel_url);
+            };
+            xhr.send();
+        } else if (l.layer_type === "XYZ") {
+            layer = L.tileLayer(l.rel_url, {
+                minZoom: 10,
+                maxZoom: 16
+            })
+        }
+        layer.addTo(map);
+        map.fitBounds(l.boundingCoordinates);
+    });
+    return mapElt;
 }
