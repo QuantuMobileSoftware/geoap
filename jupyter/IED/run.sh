b63d99bc2115ec466e4ab46718e3e52bd15b68bb
adolgarev <a.dolgarev@quantumobile.com>
2020-11-23 18:54:14 +0200

    13724: fix port

--- jupyter/IED/run.sh
+++ jupyter/IED/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -f prod.Dockerfile -t ied_nvidia_jupyter .
-docker run -d --gpus all -p 8880:8880 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_DRIVER_CAPABILITIES=all --restart always --name ied_nvidia_jupyter_1 ied_nvidia_jupyter:latest
+docker run -d --gpus all -p 8880:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_DRIVER_CAPABILITIES=all --restart always --name ied_nvidia_jupyter_1 ied_nvidia_jupyter:latest
