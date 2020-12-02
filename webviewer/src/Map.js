1132802ee5244090bcadf4ef2c0256f3951bca3b
adolgarev <a.dolgarev@quantumobile.com>
2020-12-02 12:17:14 +0200

    13926: test interactive mvt layer

--- webviewer/src/Map.js
+++ webviewer/src/Map.js
@@ -30,6 +30,24 @@ export default function createMap(widgetFactory, mapModel) {
                 zoomOffset: -1
             }).addTo(map);
         }
+
+        const mvtLayer = L.vectorGrid.protobuf("/tiles/ClearCut-2020-11-25-1/{z}/{x}/{y}.pbf", {
+            // rendererFactory: L.canvas.tile,
+            maxZoom: 16,
+            vectorTileLayerStyles: {
+                "ClearCut-2020-11-25": (properties) => {
+                    return {
+                        color: "green",
+                        fill: true
+                    }
+                }
+            },
+            interactive: true
+        }).addTo(map);
+        mvtLayer.addEventListener("click", (x) => {
+            console.log(x.layer.properties);
+        });
+        map.fitBounds([[49.8704624780525, 37.0123590916912], [50.040247115589196, 36.737865323126336]]);
     };
     var layer = null;
     mapModel.addEventListener("layerselected", () => {
