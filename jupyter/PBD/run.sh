9f9d51a299bc6ae90db21502937b10b42e71cb35
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2020-12-07 17:29:52 +0200

    13445: change for local running

--- jupyter/PBD/run.sh
+++ jupyter/PBD/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 docker build -f Dockerfile -t pbd_jupyter .
-docker run -d -p 8848:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name pbd_jupyter_1 pbd_jupyter:latest
+# docker run -d -p 8848:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name pbd_jupyter_1 pbd_jupyter:latest
+docker run -p 8848:8888 -v /home/quantum/WorkQuantum/Task07_SIP/Git/sip/data:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name pbd_jupyter_1 pbd_jupyter:latest
