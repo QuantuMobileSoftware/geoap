1dcb13de2f73595f33880f3fce26820eb8b6b278
adolgarev <a.dolgarev@quantumobile.com>
2020-12-08 12:12:34 +0200

    Allow to remove Results from admin

--- webapplication/publisher/admin.py
+++ webapplication/publisher/admin.py
@@ -28,9 +28,3 @@ class ResultAdmin(admin.OSMGeoAdmin):
             'widget': FlatJsonWidget,
         },
     }
-
-    def get_actions(self, request):
-        actions = super().get_actions(request)
-        if 'delete_selected' in actions:
-            del actions['delete_selected']
-        return actions
