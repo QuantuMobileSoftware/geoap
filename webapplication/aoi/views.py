c00309a56e2b3de03609c17decb5e15049efa491
DmitriyLugovoy <62790836+DmitriyLugovoy@users.noreply.github.com>
2020-12-11 16:48:06 +0200

    14004 permission to see unreleased layers (#46)
    * 14004_permission_to_see_unreleased_layers done, all UTs ok
    
    * 14004 remove import statment
    
    Co-authored-by: adolgarev <a.dolgarev@quantumobile.com>

--- webapplication/aoi/views.py
+++ webapplication/aoi/views.py
@@ -1,30 +1,28 @@
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
-from rest_framework import status
 from publisher.serializers import ResultSerializer
 from publisher.models import Result
 from .models import AoI
 from .serializers import AoISerializer
+from user.permissions import ModelPermissions
 
 
 class AoIListCreateAPIView(ListCreateAPIView):
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = (ModelPermissions,)
     queryset = AoI.objects.all()
     serializer_class = AoISerializer
     pagination_class = None
 
 
 class AoIRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = (ModelPermissions,)
     queryset = AoI.objects.all()
     serializer_class = AoISerializer
     http_method_names = ("get", "patch", 'delete')
 
 
 class AOIResultsListAPIView(ListAPIView):
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = (ModelPermissions,)
     serializer_class = ResultSerializer
     queryset = Result.objects.all()
     lookup_url_kwarg = "pk"
@@ -33,6 +31,6 @@ class AOIResultsListAPIView(ListAPIView):
     def get_queryset(self):
         area_of_interest = get_object_or_404(AoI, id=self.kwargs[self.lookup_url_kwarg])
         qs = self.queryset.filter(bounding_polygon__bboverlaps=area_of_interest.polygon, to_be_deleted=False)
-        if not self.request.user.is_staff:
+        if not self.request.user.has_perm('publisher.view_unreleased_result'):
             qs = qs.filter(released=True)
         return qs
--- webapplication/user/tests.py
+++ webapplication/user/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.contrib.auth.models import Group
+from .models import User
 
-# Create your tests here.
+
+class UserBase:
+    
+    @staticmethod
+    def add_users_to_groups():
+        group = Group.objects.get(name='Data_science_engineer')
+        user = User.objects.get(id=1001)
+        user.groups.add(group)
+
+        group = Group.objects.get(name='Client')
+        user = User.objects.get(id=1002)
+        user.groups.add(group)
