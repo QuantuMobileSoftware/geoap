a0dd907738c99d7e74ed3715475524dbc91a83fb
DmitriyLugovoy <62790836+DmitriyLugovoy@users.noreply.github.com>
2020-12-15 19:19:55 +0200

    13909_restrict_results_by_acl all UTs ok (#47)
    * 13909_restrict_results_by_acl all UTs ok
    
    * 13909 fix UTs, all UTs ok
    
    * 13909 fix filter name
    
    * 3909_restrict_results_by_acl
    
    	изменено:      webapplication/publisher/filters.py
    	новый файл:    webapplication/publisher/permissions.py
    	изменено:      webapplication/publisher/tests.py
    	изменено:      webapplication/publisher/views.py
    	изменено:      webapplication/user/tests.py
    
    * 13909 fix permission class name

---
+++ webapplication/publisher/permissions.py
@@ -0,0 +1,18 @@
+from rest_framework.permissions import BasePermission
+from django.core.exceptions import ObjectDoesNotExist
+from .models import ACL
+
+
+class ResultByACLPermission(BasePermission):
+    
+    def has_object_permission(self, request, view, obj):
+        try:
+            acl = ACL.objects.get(user_id=request.user.id)
+            if len(acl.restrict_projects_to) == 0:
+                return True
+            for item in acl.restrict_projects_to:
+                if str(obj.filepath).upper().startswith(item):
+                    return True
+            return False
+        except ObjectDoesNotExist:
+            return True
