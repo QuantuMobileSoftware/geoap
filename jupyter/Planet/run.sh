54446d28d7b0fa639cf0b1651bb8b41f07af2020
quantum <quantum@quantum-SIP>
2020-12-22 16:16:42 +0200

    Add password for Planet

--- jupyter/Planet/run.sh
+++ jupyter/Planet/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -f Dockerfile -t planet_jupyter .
-docker run -d -p 8849:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name planet_jupyter_1 planet_jupyter:latest
+docker run -d -p 8849:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name planet_jupyter_1 planet_jupyter:latest start-notebook.sh --NotebookApp.password='sha1:3a4d2020156f:2c3aa52c72a8cbbac85734e255c340510eb75eac'
