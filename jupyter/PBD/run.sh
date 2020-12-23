46e5996aff3b41837fe06c666048d5fc8e5174a9
adolgarev <a.dolgarev@quantumobile.com>
2020-12-23 17:49:27 +0200

    Revert "Revert "Task 14151, DS. Add GPU to PlotBoundaries", add GPU support (#58)"
    This reverts commit 766664c215d342d6903b92161036ec09d6bf507f.

--- jupyter/PBD/run.sh
+++ jupyter/PBD/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -f prod.Dockerfile -t pbd_nvidia_jupyter .
-docker run -d --shm-size=1g --gpus all -p 8848:8888 -v /mnt/disk1:/mnt/disk1 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_DRIVER_CAPABILITIES=all --restart always --name pbd_nvidia_jupyter_1 pbd_nvidia_jupyter:latest start-notebook.sh --NotebookApp.password='sha1:935d0c7b894e:e114a5b66735ae8fe59b255f42b7f92bdac84882'
+docker build -f Dockerfile -t pbd_jupyter .
+docker run -d -p 8848:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name pbd_jupyter_1 pbd_jupyter:latest start-notebook.sh --NotebookApp.password='sha1:935d0c7b894e:e114a5b66735ae8fe59b255f42b7f92bdac84882'
