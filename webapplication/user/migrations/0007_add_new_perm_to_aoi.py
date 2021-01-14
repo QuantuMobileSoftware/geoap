4f70bf387b0193931e9483d80d53587e97e8bef8
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-01-14 14:38:01 +0200

    14300 update permissions for Aoi

--- webapplication/user/migrations/0007_add_new_perm_to_aoi.py
+++ webapplication/user/migrations/0007_add_new_perm_to_aoi.py
@@ -3,6 +3,36 @@
 from django.db import migrations
 
 
+def add_permissions(apps, schema_editor):
+    """
+    Add permissions for Aoi model.
+    :param apps:
+    :param schema_editor:
+    :return:
+    """
+    permission = apps.get_model("auth", "Permission")
+    content_type = apps.get_model("contenttypes", "ContentType")
+    
+    model_def = apps.get_model("aoi", "Aoi")
+    
+    # Content type objects
+    model_content_type = content_type.objects.get_for_model(model_def)
+    
+    db_alias = schema_editor.connection.alias
+    
+    permissions_data = [
+        {"codename": "add_another_user_aoi", "name": "Can add Aoi for another user",
+         "content_type": model_content_type},
+    ]
+    
+    permission_list = []
+    for permission_data in permissions_data:
+        if not permission.objects.filter(codename=permission_data["codename"]).exists():
+            permission_list.append(permission(codename=permission_data["codename"], name=permission_data["name"],
+                                              content_type=permission_data["content_type"]))
+    permission.objects.using(db_alias).bulk_create(permission_list)
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,4 +40,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(add_permissions),
     ]
