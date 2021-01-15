9802237c2f37404550b3058f3e18246e02354c4d
DmitriyLugovoy <62790836+DmitriyLugovoy@users.noreply.github.com>
2021-01-15 17:41:38 +0200

    14300 restrict aoi to the user (#71)
    * 14300 fix CRUD for aoi
    
    * 14300 fix AOIResultsListAPIView
    
    * 14300 fix UTs
    
    * add new migration
    
    * 14300 update permissions for Aoi
    
    * 14300 add UTs
    
    * 14300 fix mistake when set wrong user_id for Aoi
    
    * 14300 remove old code

--- webapplication/aoi/views.py
+++ webapplication/aoi/views.py
@@ -1,3 +1,5 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView, ListCreateAPIView
 from rest_framework.generics import get_object_or_404
 from publisher.serializers import ResultSerializer
@@ -5,6 +7,7 @@ from publisher.models import Result
 from publisher.filters import ResultsByACLFilterBackend
 from .models import AoI, JupyterNotebook
 from .serializers import AoISerializer, JupyterNotebookSerializer
+from .permissions import AoIIsOwnerPermission
 from user.permissions import ModelPermissions
 
 
@@ -13,13 +16,34 @@ class AoIListCreateAPIView(ListCreateAPIView):
     queryset = AoI.objects.all()
     serializer_class = AoISerializer
     pagination_class = None
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user_id=self.request.user)
+        
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.initial_data['user_id'] != self.request.user.id and \
+                not self.request.user.has_perm('add_another_user_aoi'):
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class AoIRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
-    permission_classes = (ModelPermissions, )
+    permission_classes = (ModelPermissions, AoIIsOwnerPermission)
     queryset = AoI.objects.all()
     serializer_class = AoISerializer
     http_method_names = ("get", "patch", 'delete')
+    
+    def patch(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.initial_data['user_id'] != self.request.user.id and \
+                not self.request.user.has_perm('add_another_user_aoi'):
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        return self.partial_update(request, *args, **kwargs)
 
 
 class AOIResultsListAPIView(ListAPIView):
@@ -31,7 +55,7 @@ class AOIResultsListAPIView(ListAPIView):
     pagination_class = None
 
     def get_queryset(self):
-        area_of_interest = get_object_or_404(AoI, id=self.kwargs[self.lookup_url_kwarg])
+        area_of_interest = get_object_or_404(AoI, id=self.kwargs[self.lookup_url_kwarg], user_id=self.request.user)
         qs = self.queryset.filter(bounding_polygon__bboverlaps=area_of_interest.polygon)
         if not self.request.user.has_perm('publisher.view_unreleased_result'):
             qs = qs.filter(released=True)
