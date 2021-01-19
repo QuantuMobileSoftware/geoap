e33e788815648589c005b257860668d2b200fdc5
Vladyslav Yaloveha <42910560+vladyslav1997@users.noreply.github.com>
2021-01-19 15:42:54 +0200

    14310 vy multithreaded aoi command (#74)
    * 14310: add __init__
    
    * 14310: add manager
    
    * 14310: add _utils file
    
    * 14310: update notebook_manager
    
    * 14310: add _notebook
    
    * 14310: add blank line, pep8
    
    * 14310: add execute method, code upd
    
    * 14310: add request_id from file to Result table
    
    * 14310: set valid test request_ids
    
    * 14310: add to db dump test Request entries
    
    * 14310: add run for notebook_manager
    
    * 14310: add blank lines
    
    * 14391: replace logging error to warning
    
    * 14391: update start time before sleep
    
    * 14391: rename to notebook executor
    
    * Revert "14310: add to db dump test Request entries"
    
    This reverts commit 682686a5
    
    * 14310: comment request ids generation

---
+++ webapplication/aoi/management/commands/notebook_executor.py
@@ -0,0 +1,39 @@
+import logging
+import sys
+from aoi.management.commands._notebook import State, NotebookThread
+from multiprocessing import Process
+from django.core.management.base import BaseCommand
+
+logger = logging.getLogger(__name__)
+
+THREADS = 2
+TIMEOUT = 60 * 60 * 2
+
+
+class Command(BaseCommand):
+    help = "Manage running of Jupyter Notebooks by client's request"
+
+    def handle(self, *args, **options):
+        exitcode = None
+        while exitcode == 1 or exitcode is None:
+            child_process = Process(target=self.run, daemon=True)
+            child_process.start()
+            child_process.join()
+            exitcode = child_process.exitcode
+
+    def run(self):
+        state = State()
+
+        threads = [NotebookThread(state, daemon=True) for _ in range(THREADS)]
+
+        logger.info(f"Created {len(threads)} threads")
+
+        for thread in threads:
+            thread.start()
+
+        while True:
+            for thread in threads:
+                thread.join(TIMEOUT)
+                if not thread.is_alive():
+                    logger.error('One of threads exited - terminate and restart')
+                    sys.exit(1)
