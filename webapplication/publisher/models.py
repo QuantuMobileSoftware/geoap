aefb4252c970d3e3ff9349ebfb7d1ebd563dac2d
vladyslav1997 <vladyslavyaloveha@gmail.com>
2021-01-21 16:04:53 +0200

    14400: change verbose_names in models

--- webapplication/publisher/models.py
+++ webapplication/publisher/models.py
@@ -47,7 +47,7 @@ class Result(models.Model):
 
 
 class ACL(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User name')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='User id')
     restrict_projects_to = ArrayField(models.CharField(max_length=20), blank=True, verbose_name='Restrict projects to')
 
     def __str__(self):
