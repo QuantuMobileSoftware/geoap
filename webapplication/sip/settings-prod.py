607982ced2d3443a928dfa4d2af1c1543858a38c
adolgarev <a.dolgarev@quantumobile.com>
2021-01-29 12:39:35 +0200

    Fix: detail: CSRF Failed: Referer checking failed - https://soilmate.xyz/ does not match any trusted origins.

--- webapplication/sip/settings-prod.py
+++ webapplication/sip/settings-prod.py
@@ -5,6 +5,7 @@ DEBUG = False
 # AOI app, notebook_executor
 NOTEBOOK_EXECUTOR_GPUS = "all"
 
-ALLOWED_HOSTS.append("192.168.1.61", "soilmate.xyz")
+ALLOWED_HOSTS.append("192.168.1.61")
+ALLOWED_HOSTS.append("soilmate.xyz")
 
 CSRF_TRUSTED_ORIGINS = ["soilmate.xyz"]
