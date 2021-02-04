41fbec4eed368d70595e9fec93e3c086ea07cb6b
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2021-02-04 14:53:53 +0200

    14456: add call edit method

--- webapplication/aoi/management/commands/_notebook.py
+++ webapplication/aoi/management/commands/_notebook.py
@@ -73,6 +73,7 @@ class NotebookThread(Thread):
             request.save()
 
             with ContainerExecutor(request) as ce:
+                ce.edit()
                 success = ce.execute()
 
         except Exception as ex:
