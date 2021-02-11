55aedc2d326ef00bde1709257272695a2f24fe47
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2021-02-11 16:30:03 +0200

    14509: add run.sh, config file to example_jupyter nb

---
+++ jupyter/example/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker build -f Dockerfile -t example_jupyter .
+docker run -d -p 8847:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name example_jupyter_1 example_jupyter:latest start-notebook.sh --NotebookApp.password='sha1:c13a1700f710:d5735a5cc1d95e9f8e8ae6691b6e647d38e3d852'
