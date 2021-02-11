3bba3135594fbdf6493eb27a41e4b028cfe468a0
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-02-11 18:28:59 +0200

    14584 split GPU usage

--- jupyter/PBDNN/run.sh
+++ jupyter/PBDNN/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -f prod.Dockerfile -t pbd_nvidia_jupyter .
-docker run -d --shm-size=1g --gpus all -p 8850:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_DRIVER_CAPABILITIES=all --restart always --name pbd_nvidia_jupyter_1 pbd_nvidia_jupyter:latest
+docker run -d --shm-size=1g --gpus device=1 -p 8850:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_DRIVER_CAPABILITIES=all --restart always --name pbd_nvidia_jupyter_1 pbd_nvidia_jupyter:latest
--- jupyter/PW/run.sh
+++ jupyter/PW/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -f prod.Dockerfile -t pw_nvidia_jupyter .
-docker run -d --shm-size=1g --gpus all -p 8888:8888 -v /mnt/disk1:/mnt/disk1 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_DRIVER_CAPABILITIES=all --restart always --name pw_nvidia_jupyter_1 pw_nvidia_jupyter:latest
+docker run -d --shm-size=1g --gpus device=1 -p 8888:8888 -v /mnt/disk1:/mnt/disk1 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes -e NVIDIA_DRIVER_CAPABILITIES=all --restart always --name pw_nvidia_jupyter_1 pw_nvidia_jupyter:latest
