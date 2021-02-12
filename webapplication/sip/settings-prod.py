825a7563fef3c4e51e125aecf1eec8bfb53dc449
Vladyslav Yaloveha <42910560+vladyslav1997@users.noreply.github.com>
2021-02-12 13:03:47 +0200

    14509 host volumes update (#98)
    * 14509: fix settings import
    
    * 14509: add host volumes basenames to settings
    
    * 14509: add basename for exception raising
    
    * 14509: add prod volume with webapplication contents
    
    * 14509: rm redeclaration for HOST_VOLUME_WEBAPPLICATION_BASENAME
    
    Co-authored-by: Vladyslav Yaloveha <v.yaloveha@quantumobile.com>

--- webapplication/sip/settings-prod.py
+++ webapplication/sip/settings-prod.py
@@ -2,12 +2,12 @@ from sip.settings import *
 
 DEBUG = False
 
-# AOI app, notebook_executor
-NOTEBOOK_EXECUTOR_GPUS = "all"
-
 ALLOWED_HOSTS.append("192.168.1.61")
 ALLOWED_HOSTS.append("soilmate.xyz")
 
 CSRF_TRUSTED_ORIGINS = ["soilmate.xyz"]
 
+# AOI app, notebook_executor
+NOTEBOOK_EXECUTOR_GPUS = "all"
 BASE_CONTAINER_NAME = "sip_code_webapplication_1"
+HOST_VOLUME_DATA_BASENAME = "sip"
