6ad4cf4c5ec901ab88f46de8cc3350e68acae2ec
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2021-02-12 12:38:43 +0200

    14509: add host volumes basenames to settings

--- webapplication/sip/settings-prod.py
+++ webapplication/sip/settings-prod.py
@@ -2,12 +2,13 @@ from sip.settings import *
 
 DEBUG = False
 
-# AOI app, notebook_executor
-NOTEBOOK_EXECUTOR_GPUS = "all"
-
 ALLOWED_HOSTS.append("192.168.1.61")
 ALLOWED_HOSTS.append("soilmate.xyz")
 
 CSRF_TRUSTED_ORIGINS = ["soilmate.xyz"]
 
+# AOI app, notebook_executor
+NOTEBOOK_EXECUTOR_GPUS = "all"
 BASE_CONTAINER_NAME = "sip_code_webapplication_1"
+HOST_VOLUME_WEBAPPLICATION_BASENAME = "webapplication"
+HOST_VOLUME_DATA_BASENAME = "sip"
