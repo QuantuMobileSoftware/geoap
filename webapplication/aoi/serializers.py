11d256e7cac1b95470cf858b9732f4c92e811ff2
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-02-15 21:22:02 +0200

    14688 update model and db.dump, add polygon field to request serializer

--- webapplication/aoi/serializers.py
+++ webapplication/aoi/serializers.py
@@ -24,4 +24,5 @@ class RequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = Request
         fields = ('id', 'user', 'aoi', 'notebook', 'notebook_name',
-                  'date_from', 'date_to', 'started_at', 'finished_at', 'error', 'success', )
+                  'date_from', 'date_to', 'started_at', 'finished_at', 'error', 'success', 'polygon',)
+        read_only_fields = ['polygon', ]
