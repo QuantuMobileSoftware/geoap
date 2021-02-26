f5f471dd9f84569522e4b511327521606b54132a
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2021-02-26 16:49:22 +0200

    14786: add threads stopping if one shutdown

--- webapplication/aoi/management/commands/notebook_executor.py
+++ webapplication/aoi/management/commands/notebook_executor.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import time
 from aoi.management.commands._notebook import State, NotebookThread, PublisherThread
 from multiprocessing import Process
 from django.core.management.base import BaseCommand
@@ -7,13 +8,15 @@ from django.conf import settings
 
 logger = logging.getLogger(__name__)
 
+THREAD_SLEEP = 10
+
 
 class Command(BaseCommand):
     help = "Manage running of Jupyter Notebooks by client's request"
 
     def handle(self, *args, **options):
         exitcode = None
-        while exitcode == 1 or exitcode is None:
+        while exitcode == 2 or exitcode is None:
             child_process = Process(target=self.run, daemon=True)
             child_process.start()
             child_process.join()
@@ -30,9 +33,19 @@ class Command(BaseCommand):
         for thread in threads:
             thread.start()
 
-        while True:
+        # main thread looks at the status of all threads
+        try:
+            while True:
+                for thread in threads:
+                    if thread.exception:
+                        # an error in a thread - raise it in main thread too
+                        logger.error(f"Thread: {thread} exited: {thread.exception}.Terminate all threads and restart")
+                        raise thread.exception
+                time.sleep(THREAD_SLEEP)
+        except:
+            logger.info(f"Stopping all threads...")
             for thread in threads:
-                thread.join(settings.NOTEBOOK_EXECUTOR_TIMEOUT)
-                if not thread.is_alive():
-                    logger.error('One of threads exited - terminate and restart')
-                    sys.exit(1)
+                thread.stop()
+            logger.info(f"All threads stopped. Restart command")
+            sys.exit(2)
+
