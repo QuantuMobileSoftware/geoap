b975e0202d439e2deaaf9491d6482a6be11754c3
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-03-01 15:17:21 +0200

    14172 update PR to latest master

--- webapplication/publisher/migrations/0017_result_styles_url.py
+++ webapplication/publisher/migrations/0017_result_styles_url.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('publisher', '0009_acl'),
+        ('publisher', '0016_change_verbose_name'),
     ]
 
     operations = [
