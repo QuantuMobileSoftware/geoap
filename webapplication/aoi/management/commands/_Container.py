cb9f729f830765d7f35f6eb4bcc8c07d4ff63d20
Vladyslav Yaloveha <v.yaloveha@quantumobile.com>
2021-03-04 17:57:01 +0200

    14786: add checking exec_run return

--- webapplication/aoi/management/commands/_Container.py
+++ webapplication/aoi/management/commands/_Container.py
@@ -120,7 +120,7 @@ class ContainerExecutor(Container):
                       {kernel}
                       """
 
-        exit_code, (_, stderr) = self.container.exec_run(command, demux=True)
+        exit_code, (stdout, stderr) = self.container.exec_run(command, demux=True)
 
         logger.info(f"Request: {self.request.pk}: Finished executing notebook {self.notebook.name}, "
                     f"exit code: {exit_code}")
@@ -128,5 +128,11 @@ class ContainerExecutor(Container):
         if exit_code == 0:
             return True
         else:
-            logger.error(f"Request {self.request.pk}: {self.notebook.name}: {stderr.decode('utf-8')}")
+            if stderr:
+                message = stderr.decode('utf-8')
+            elif stdout:
+                message = stdout.decode('utf-8')
+            else:
+                message = None
+            logger.error(f"Request {self.request.pk}: {self.notebook.name}: {message}")
             return False
