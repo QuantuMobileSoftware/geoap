df6558869755e458fb02ccbb989d7b84b5049146
demenskiy <yaroslav.demenskiy@gmail.com>
2021-05-13 10:39:17 +0300

    15282: Initial setup

---
+++ soilmate_frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<React.StrictMode></React.StrictMode>, document.getElementById('root'));
+
+reportWebVitals();
