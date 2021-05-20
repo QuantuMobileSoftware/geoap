c25970508049f3606b79520f2052e5e5c9760838
demenskiy <yaroslav.demenskiy@gmail.com>
2021-05-20 16:39:43 +0300

    15282: Areas API requests added

--- soilmate_frontend/src/state/areas/areas.actions.js
+++ soilmate_frontend/src/state/areas/areas.actions.js
@@ -5,6 +5,7 @@ import { API } from 'api';
 
 import { areasActions } from './areas.slice';
 import { useAsync } from 'hooks';
+import { normalizeAreas } from 'utils';
 
 export const useAreasActions = () => {
   const dispatch = useDispatch();
@@ -12,10 +13,22 @@ export const useAreasActions = () => {
 
   const getAreas = useCallback(async () => {
     await handleAsync(async () => {
-      const getAreasResponse = await API.areas.getAreas();
-      console.log(getAreasResponse);
+      const areas = (await API.areas.getAreas()).data;
+
+      const areasWithFields = await Promise.all(
+        areas.map(async area => {
+          const [requests, results] = await Promise.all([
+            API.areas.getAreaRequests(area.id),
+            API.areas.getAreaResults(area.id)
+          ]).then(responses => responses.map(({ data }) => data));
+
+          return { ...area, requests, results };
+        })
+      );
+
+      dispatch(areasActions.setEntities(normalizeAreas(areasWithFields)));
     });
-  }, [handleAsync]);
+  }, [dispatch, handleAsync]);
 
   return { isLoading, error, getAreas };
 };
