689b47c2f598401e339a066dbbce16f9aa5f9bd8
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-06-24 18:29:21 +0300

    15713/add shape creating

--- soilmate_frontend/src/components/Map/components/MapPolygon.jsx
+++ soilmate_frontend/src/components/Map/components/MapPolygon.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Polygon } from 'react-leaflet';
+import { SHAPE_OPTIONS } from '_constants';
 
 export const MapPolygon = ({ map, coordinates, isActive = false }) => {
   const circleRef = useRef();
@@ -11,9 +12,10 @@ export const MapPolygon = ({ map, coordinates, isActive = false }) => {
     <Polygon
       positions={coordinates}
       ref={circleRef}
-      pathOptions={{ color: '#e3e3e3', fillColor: '#ffffff' }}
+      pathOptions={SHAPE_OPTIONS}
       eventHandlers={{
         click: () => {
+          console.log(circleRef.current);
           map
             .panTo(circleRef.current.getCenter())
             .fitBounds(circleRef.current.getBounds());
