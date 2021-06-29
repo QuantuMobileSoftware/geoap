0261cdd1b66a414902e7a111f1014c813488c2a3
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-06-29 16:42:31 +0300

    15713/refactor

--- soilmate_frontend/src/components/_shared/Popup/Popup.jsx
+++ soilmate_frontend/src/components/_shared/Popup/Popup.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyledPopup, Header } from './Popup.styled';
 import { Button } from '../Button';
 
-export const Popup = ({ header, confirmText, cancel, save }) => (
+export const Popup = ({ header, confirmPopup, cancel, save }) => (
   <StyledPopup>
     <Header>{header}</Header>
     <Button variant='secondary' onClick={cancel}>
       Cancel
     </Button>
     <Button variant='primary' onClick={save}>
-      {confirmText}
+      {confirmPopup}
     </Button>
   </StyledPopup>
 );
