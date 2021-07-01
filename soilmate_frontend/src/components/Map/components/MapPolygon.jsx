8c0bdbd738ce22fc19008d74a11c2504fb886f5d
vlad <vladbashilov@icloud.com>
2021-07-01 11:23:13 +0300

    Fix polygon click and list item click

--- soilmate_frontend/src/components/Map/components/MapPolygon.jsx
+++ soilmate_frontend/src/components/Map/components/MapPolygon.jsx
@@ -1,31 +1,17 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from 'react';
 import { Polygon } from 'react-leaflet';
 
-import { useAreasActions } from 'state';
 import { SHAPE_OPTIONS } from '_constants';
 
-export const MapPolygon = ({ map, coordinates, isActive = false }) => {
-  const circleRef = useRef();
-  const { setCurrentArea } = useAreasActions();
-
-  const center = circleRef.current && circleRef.current.getCenter();
-  const bounds = circleRef.current && circleRef.current.getBounds();
-
-  useEffect(() => {
-    if (isActive) {
-      map.panTo(center).fitBounds(bounds);
-    }
-  }, [map, center, bounds, isActive]);
-
+export const MapPolygon = ({ coordinates, onClick }) => {
+  const polyRef = useRef(null);
   return (
     <Polygon
+      ref={polyRef}
       positions={coordinates}
-      ref={circleRef}
       pathOptions={SHAPE_OPTIONS}
       eventHandlers={{
-        click: () => {
-          map.panTo(center).fitBounds(bounds);
-        }
+        click: () => onClick(polyRef.current)
       }}
     />
   );
