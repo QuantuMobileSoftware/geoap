088ad938ae9754fc6fa4da9bcb54634a20a39f73
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-07-20 13:40:08 +0300

    15933/add datepicker

--- soilmate_frontend/src/components/Areas/Sidebar/RequestSettings/RequestSettings.jsx
+++ soilmate_frontend/src/components/Areas/Sidebar/RequestSettings/RequestSettings.jsx
@@ -1,10 +1,11 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useRef } from 'react';
 import { Select } from 'components/_shared/Select';
 import { Button } from 'components/_shared/Button';
 import { SIDEBAR_MODE } from '_constants';
 import { useAreasActions, selectLayers } from 'state';
-import { ButtonWrapper } from './RequestSettings.styles';
+import { ButtonWrapper, StyledDatePicker, ApplyButton } from './RequestSettings.styles';
 import { useSelector } from 'react-redux';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const startYear = 2015;
 const layerYears = Array.from({ length: new Date().getFullYear() - startYear + 1 }).map(
@@ -14,6 +15,11 @@ const layerYears = Array.from({ length: new Date().getFullYear() - startYear + 1
 export const RequestSettings = ({ areas, currentArea }) => {
   const { setSidebarMode } = useAreasActions();
   const layers = useSelector(selectLayers);
+  const calendarRef = useRef(null);
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(null);
+
   const selectOptionsAreas = useMemo(
     () => areas.map(({ name }) => ({ name, value: name })),
     [areas]
@@ -23,11 +29,39 @@ export const RequestSettings = ({ areas, currentArea }) => {
     [layers]
   );
 
+  const onChange = dates => {
+    const [start, end] = dates;
+    setStartDate(start);
+    setEndDate(end);
+  };
+
   return (
     <>
       <Select items={selectOptionsAreas} value={currentArea.name} label='Choose area' />
       <Select items={selectOptionsLayers} label='Select layers' />
       <Select items={layerYears} label='Year' />
+      <StyledDatePicker
+        selected={startDate}
+        onChange={onChange}
+        startDate={startDate}
+        endDate={endDate}
+        selectsRange
+        shouldCloseOnSelect={false}
+        calendarContainer={({ children }) => {
+          return (
+            <div>
+              <div>{children}</div>
+              <ApplyButton
+                variant='primary'
+                onClick={() => calendarRef.current.setOpen(false)}
+              >
+                apply
+              </ApplyButton>
+            </div>
+          );
+        }}
+        ref={calendarRef}
+      />
 
       <ButtonWrapper>
         <Button
@@ -38,9 +72,7 @@ export const RequestSettings = ({ areas, currentArea }) => {
         >
           Back to list
         </Button>
-        <Button variant='primary' onClick={() => {}}>
-          Save changes
-        </Button>
+        <Button variant='primary'>Save changes</Button>
       </ButtonWrapper>
     </>
   );
