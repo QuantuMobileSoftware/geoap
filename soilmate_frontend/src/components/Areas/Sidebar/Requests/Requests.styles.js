ea25da0fb6619fff21670c1d6ce37446ee08e500
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-07-28 18:04:34 +0300

    16019/add tabs in reports

--- soilmate_frontend/src/components/Areas/Sidebar/Requests/Requests.styles.js
+++ soilmate_frontend/src/components/Areas/Sidebar/Requests/Requests.styles.js
@@ -2,6 +2,8 @@ import styled, { css } from 'styled-components';
 import { em } from 'styles';
 import { Typography } from 'components/_shared/Typography';
 
+import { Icon } from 'components/_shared/Icon';
+
 export const RequestsSidebarMessage = styled(Typography).attrs({
   element: 'p',
   variant: 'body2'
@@ -20,5 +22,31 @@ export const ButtonWrapper = styled.div`
       margin-right: ${em(11)};
       border: ${theme.borders.default(theme.fontSizes[2])};
     }
+  `}
+`;
+
+export const StyledIcon = styled(Icon)`
+  display: inline-flex;
+  transform: rotate(${props => (props.up ? `0` : `180deg`)});
+`;
+
+export const ButtonTopWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: ${em(16)};
+`;
+
+export const TabsWrapper = styled.div`
+  display: flex;
+`;
+
+export const TabItem = styled.div`
+  ${({ theme, isActive }) => css`
+    width: 50%;
+    border-bottom: ${theme.borders.default({ fontSize: theme.fontSizes[1] })};
+    color: ${isActive ? theme.colors.primary.p2 : theme.colors.nature.n3};
+    border-color: ${isActive ? theme.colors.primary.p2 : theme.colors.nature.n3};
+    cursor: pointer;
+    text-align: center;
   `}
 `;
