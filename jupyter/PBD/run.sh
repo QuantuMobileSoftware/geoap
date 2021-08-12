f863bcb14aaeb8b7ecf75e69c869a1b08babfe88
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-08-12 17:25:00 +0300

    16026 made pretty jupyter/PBD/run.sh

--- jupyter/PBD/run.sh
+++ jupyter/PBD/run.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 docker build -f Dockerfile -t pbd_jupyter .
-docker run -d -p 8848:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name pbd_jupyter_1 pbd_jupyter:latest start-notebook.sh --NotebookApp.password='sha1:935d0c7b894e:e114a5b66735ae8fe59b255f42b7f92bdac84882'
+docker run -d -p 8848:8888 -v /home/quantum/sip:/home/jovyan/work \
+  -e JUPYTER_ENABLE_LAB=yes --restart always --name pbd_jupyter_1 pbd_jupyter:latest \
+  start-notebook.sh --NotebookApp.password='sha1:935d0c7b894e:e114a5b66735ae8fe59b255f42b7f92bdac84882'
