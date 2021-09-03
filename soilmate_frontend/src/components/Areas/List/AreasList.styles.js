85525315c5efeb3a6cec20d403b172838dc5d03c
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-09-03 14:09:39 +0300

    16404/change areasList prop name

--- soilmate_frontend/src/components/Areas/List/AreasList.styles.js
+++ soilmate_frontend/src/components/Areas/List/AreasList.styles.js
@@ -3,10 +3,10 @@ import styled, { css } from 'styled-components';
 import { rem, sidebarList } from 'styles';
 
 export const AreasList = styled.ul`
-  ${({ theme, items }) => css`
+  ${({ theme, areasAmount }) => css`
     ${sidebarList}
     margin: 0 -${rem(theme.spacing[8])};
     margin-top: ${rem(theme.spacing[2])};
-    overflow-y: ${items > 1 ? 'auto' : 'visible'};
+    overflow-y: ${areasAmount > 1 ? 'auto' : 'visible'};
   `}
 `;
