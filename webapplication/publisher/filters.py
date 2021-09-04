e84dad74dd196d6853f39dfc8328ed4e79139a41
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-09-04 13:34:14 +0300

    16401 added docstring,  print statements were removed

--- webapplication/publisher/filters.py
+++ webapplication/publisher/filters.py
@@ -14,7 +14,9 @@ def filter_for_results_by_acl(restrict_projects_to):
 
 class ResultsByACLFilterBackend(filters.BaseFilterBackend):
     """
-    Filter that only allows users to see Result objects if Result.filepath starts with any of items stored in
+    Filter Result objects that were created by user requests,
+    and
+    filter Result objects with null request and Result.filepath starts with any of items stored in
     ACL.restrict_projects_to field and ACL.user == request.user.id,
     or
     request.user.id not in ACL table,
@@ -25,19 +27,12 @@ class ResultsByACLFilterBackend(filters.BaseFilterBackend):
         try:
             acl = ACL.objects.get(user=request.user.id)
             if len(acl.restrict_projects_to) == 0:
-                print('len ACL = 0')
                 queryset = queryset.filter(Q(request__isnull=True) | Q(request__user__id=request.user.id))
-                print(queryset)
                 return queryset
-
-            print(f'len ACL = {len(acl.restrict_projects_to)}')
             query = filter_for_results_by_acl(acl.restrict_projects_to)
             queryset = queryset.filter(query | Q(request__user__id=request.user.id))
-            print(queryset)
             return queryset
         
         except ObjectDoesNotExist:
-            print('ACL is None')
             queryset = queryset.filter(Q(request__isnull=True) | Q(request__user__id=request.user.id))
-            print(queryset)
             return queryset
