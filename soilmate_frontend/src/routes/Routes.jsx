65070a1ab352baadd79d32ff4334a0bf1b5d9ac8
Stanislav <35593946+StanislavKharkiv@users.noreply.github.com>
2021-09-20 15:15:10 +0300

    16661/add hotjar (#227)
    * 16661/add hotjar
    
    * 16661/add hotjar from process.env

--- soilmate_frontend/src/routes/Routes.jsx
+++ soilmate_frontend/src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { Switch } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { hotjar } from 'react-hotjar';
 
 import { ROUTES } from '_constants';
 import { useUserActions, selectIsAuthorized } from 'state';
@@ -8,15 +9,27 @@ import { useUserActions, selectIsAuthorized } from 'state';
 import { Route } from './Route';
 import { PageAuth, PageMain } from 'pages';
 
+const {
+  REACT_APP_AUTOLOGIN,
+  REACT_APP_AUTOPASSWORD,
+  REACT_APP_HOTJAR_ID,
+  REACT_APP_HOTJAR_SV
+} = process.env;
+
 export const Routes = () => {
   const { login } = useUserActions();
   const isAuthorized = useSelector(selectIsAuthorized);
 
   useEffect(() => {
+    if (REACT_APP_HOTJAR_ID && REACT_APP_HOTJAR_SV) {
+      hotjar.initialize(REACT_APP_HOTJAR_ID, REACT_APP_HOTJAR_SV);
+    }
+  }, []);
+
+  useEffect(() => {
     if (isAuthorized) {
       return;
     }
-    const { REACT_APP_AUTOLOGIN, REACT_APP_AUTOPASSWORD } = process.env;
     if (REACT_APP_AUTOLOGIN && REACT_APP_AUTOPASSWORD) {
       login({
         username: REACT_APP_AUTOLOGIN,
