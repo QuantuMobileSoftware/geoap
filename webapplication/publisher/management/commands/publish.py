a75825cdb64ae432977975943cb26d1acd57132a
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-09-21 10:33:55 +0300

    16667 fixed ref to settings in publish.py

--- webapplication/publisher/management/commands/publish.py
+++ webapplication/publisher/management/commands/publish.py
@@ -36,7 +36,7 @@ def rm_empty_dirs():
     """
     logger.info('Removing of empty directories started ')
     command = ["find", settings.TILES_FOLDER, "-empty", "-type", "d", "-delete"]
-    File.run_process(command, settings.MAX_TIMEOUT_FOR_TILE_CREATION_SECONDS)
+    File.run_process(command, settings.MAX_TIMEOUT_FOR_TILES_FOLDER_CLEANING)
     logger.info('Removing of empty directories finished ')
     return
 
