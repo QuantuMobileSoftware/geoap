a40ace57e30276319e84dac330bacbb088c3acce
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-09-23 16:35:11 +0300

    16697/add error modal

---
+++ soilmate_frontend/src/_constants/errors.js
@@ -0,0 +1,4 @@
+export const PERMISSION_ERROR = 'You have no permissions for this activity';
+export const SERVER_ERROR = "Sorry, we've got a server error, please try a bit later";
+export const FILE_ERROR =
+  'Sorry, we can not process your file. Please check its size/format';
--- soilmate_frontend/src/components/App/App.jsx
+++ soilmate_frontend/src/components/App/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import { ThemeProvider } from 'styles';
 import { Routes } from 'routes';
+import { ErrorModal } from 'components/ErrorModal';
 
 export const App = () => {
   return (
     <ThemeProvider>
       <BrowserRouter>
         <Routes />
+        <ErrorModal />
       </BrowserRouter>
     </ThemeProvider>
   );
