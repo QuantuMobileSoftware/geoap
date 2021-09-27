5ce53824f9cdeb51946026988b80772609237af6
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-09-27 12:56:36 +0300

    16407/show color map

--- soilmate_frontend/src/components/Map/components/MapColorBar/MapColorBar.styles.js
+++ soilmate_frontend/src/components/Map/components/MapColorBar/MapColorBar.styles.js
@@ -5,15 +5,27 @@ import { rem, mapControls } from 'styles';
 export const ColorBarWrapper = styled.div`
   ${mapControls};
   top: ${rem(30)};
-  width: ${rem(140)};
+  width: ${rem(260)};
   padding: ${rem(18)} ${rem(10)};
 `;
 
 export const ColorBar = styled.div`
   ${({ colors, theme }) => css`
+    margin-top: ${rem(8)};
+    margin-bottom: ${rem(4)};
     width: 100%;
-    height: ${rem(4)};
+    height: ${rem(8)};
     background: linear-gradient(to right, ${colors});
     border-radius: ${rem(theme.radius[1])};
   `}
+`;
+
+export const ColorBarTitle = styled.div`
+  font-size: ${rem(10)};
+`;
+
+export const ColorBarLabels = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: ${rem(8)};
 `;
