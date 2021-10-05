0bda1405ed52753fd927b377537c15707a5ce8c2
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-10-05 12:37:53 +0300

    16907/fix contacts, colormap results

--- soilmate_frontend/src/components/Map/Map.jsx
+++ soilmate_frontend/src/components/Map/Map.jsx
@@ -63,7 +63,8 @@ export const Map = () => {
   const isLoading = useSelector(getLoading);
   const selectedResults = useSelector(getSelectedResults);
   const activeTab = useSelector(selectRequestTab);
-  const { addNewArea, setCurrentArea, setSidebarMode } = useAreasActions();
+  const { addNewArea, setCurrentArea, setSidebarMode, deleteSelectedResult } =
+    useAreasActions();
 
   const isCreatedTab = activeTab === REQUEST_TABS.CREATED;
   const colorMap = useMemo(() => {
@@ -172,11 +173,11 @@ export const Map = () => {
   };
 
   const handlePolygonClick = id => polygon => {
-    const center = polygon.getCenter();
-    const bounds = polygon.getBounds();
-    map.panTo(center).fitBounds(bounds);
+    if (id === currentAreaId) return;
     setSidebarMode(selectedArea.type === AOI_TYPE.AREA ? AREAS : FIELDS);
     setCurrentArea(id);
+    deleteSelectedResult();
+    map.panTo(polygon.getCenter()).fitBounds(polygon.getBounds());
   };
 
   return (
