09de6a0c58fefe562d8653159c1a82e94c8c36fe
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2021-10-07 17:39:37 +0300

    16748 added FieldFromResultCreateAPIView endpoint

--- webapplication/publisher/urls.py
+++ webapplication/publisher/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (FilesView, ResultListAPIView, ResultRetrieveUpdateDestroyAPIView)
+from .views import (FilesView, ResultListAPIView, ResultRetrieveUpdateDestroyAPIView, FieldFromResultCreateAPIView)
 
 urlpatterns = [
     path('files/<path:file_path>', FilesView.as_view(), name='get_file'),
     path('results', ResultListAPIView.as_view(), name='get_results'),
     path('results/<int:pk>', ResultRetrieveUpdateDestroyAPIView.as_view(), name='result'),
+    path('results/<int:pk>/field/', FieldFromResultCreateAPIView.as_view(), name='field_from_result'),
 ]
--- webapplication/publisher/views.py
+++ webapplication/publisher/views.py
@@ -1,12 +1,17 @@
+import geopandas as gpd
+from shapely.geometry import Point
+from pathlib import Path
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework import status
+from django.conf import settings
+from rest_framework.exceptions import APIException
 from django.http import HttpResponse
 from .models import Result
-from .serializers import ResultSerializer
+from .serializers import ResultSerializer, PointSerializer
 from .filters import ResultsByACLFilterBackend
 from .permissions import ResultByACLPermission
 from user.permissions import ModelPermissions
@@ -95,3 +100,41 @@ class ResultRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
             result.save()
             return Response(status=status.HTTP_204_NO_CONTENT)
         return Response(status=status.HTTP_403_FORBIDDEN)
+
+
+class FieldFromResultCreateAPIView(CreateAPIView):
+    permission_classes = (IsAuthenticated, ResultByACLPermission)
+    queryset = Result.objects.all()
+    serializer_class = PointSerializer
+    lookup_url_kwarg = "pk"
+    
+    def get_queryset(self):
+        if self.request.user.has_perm('publisher.view_unreleased_result'):
+            return self.queryset
+        return self.queryset.filter(released=True)
+    
+    def create(self, request, *args, **kwargs):
+        result = self.get_object()
+        point_serializer = self.get_serializer(data=request.data)
+        point_serializer.is_valid(raise_exception=True)
+        point = Point(point_serializer.validated_data['lat'], point_serializer.validated_data['lng'])
+        try:
+            df = gpd.read_file(Path(settings.RESULTS_FOLDER) / result.filepath)
+        except Exception as ex:
+            print(f"Cannot read file {result.filepath}: {str(ex)}")
+            raise APIException(detail=f'result with id {self.kwargs[self.lookup_url_kwarg]} \
+            does not contain valid geojson')
+
+        df['disjoint'] = df.disjoint(point)
+        output = df.loc[df['disjoint'] != True]
+        if len(output) > 1:
+            raise APIException(detail=f'result with id {self.kwargs[self.lookup_url_kwarg]} contains {len(output)} \
+            polygons that are contain {point}')
+
+        if len(output) == 0:
+            raise APIException(detail=f'result with id {self.kwargs[self.lookup_url_kwarg]} does not contain any \
+            polygon that is contain {point}')
+
+        polygon = f"SRID={output.crs.srs.split(':')[1]};{output.iloc[0]['geometry'].wkt}"
+        headers = self.get_success_headers(point_serializer.data)
+        return Response({'polygon': polygon}, status=status.HTTP_200_OK, headers=headers)
