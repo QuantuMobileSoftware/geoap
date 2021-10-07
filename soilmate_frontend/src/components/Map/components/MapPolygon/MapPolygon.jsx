02bf4b4340bb69400668aa0293d2194389061de7
StanislavKharkiv <s.iosifov@quantumobile.com>
2021-10-07 18:13:43 +0300

    16928/get coords from MKV boundaries fields

--- soilmate_frontend/src/components/Map/components/MapPolygon/MapPolygon.jsx
+++ soilmate_frontend/src/components/Map/components/MapPolygon/MapPolygon.jsx
@@ -1,15 +1,53 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import L from 'leaflet';
 import { Polygon } from 'react-leaflet';
 import { useMapActions, useAreasActions } from 'state';
 import { SHAPE_OPTIONS, SELECTED_SHAPE_OPTIONS } from '_constants';
 
-export const MapPolygon = ({ coordinates, onClick, isEditable, id, currentAreaId }) => {
+export const MapPolygon = ({
+  coordinates,
+  onClick,
+  isEditable,
+  id,
+  currentAreaId,
+  currentAreaIndex,
+  selectedResult
+}) => {
   const polyRef = useRef(null);
   const { setEditableShape } = useMapActions();
   const { setEntitySize } = useAreasActions();
+  const [prevAreaId, setPrevAreaId] = useState(currentAreaId);
+  const [hasMvtResult, setHasMvtResult] = useState(false);
 
   const pathOptions = currentAreaId === id ? SELECTED_SHAPE_OPTIONS : SHAPE_OPTIONS;
+
+  useEffect(() => {
+    if (!prevAreaId) {
+      setPrevAreaId(currentAreaId);
+    }
+    if (hasMvtResult && prevAreaId !== currentAreaId) {
+      polyRef.current
+        .getPane()
+        .querySelectorAll('svg path')
+        .forEach(item => (item.style.pointerEvents = 'auto'));
+      setPrevAreaId(currentAreaId);
+      setHasMvtResult(false);
+    }
+  }, [currentAreaId, prevAreaId, hasMvtResult]);
+
+  useEffect(() => {
+    if (
+      currentAreaId === id &&
+      currentAreaIndex >= 0 &&
+      selectedResult?.layer_type === 'MVT' &&
+      !hasMvtResult
+    ) {
+      polyRef.current.getPane().querySelectorAll('svg path')[
+        currentAreaIndex
+      ].style.pointerEvents = 'none';
+      setHasMvtResult(true);
+    }
+  }, [currentAreaIndex, currentAreaId, id, selectedResult, hasMvtResult]);
 
   useEffect(() => {
     if (polyRef.current) {
