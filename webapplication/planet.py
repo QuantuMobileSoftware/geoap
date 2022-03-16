40bb08dddca54d23a1257c18c7f069451d8e9fcf
Michael Yushchuk <m.yushchuk@quantumobile.com>
2022-03-16 11:18:02 +0200

    added visual skysat processing

--- webapplication/planet.py
+++ webapplication/planet.py
@@ -24,12 +24,28 @@ def transform_raster(raster_path, new_raster_path, date, request_id=0):
                 dst.write(cur_band, band)
 
 
-def merge_rasters(raster_path_list, date, save_path):
+def transform_raster_visual(raster_path, new_raster_path, date, request_id=0):
+    """
+    For Visual SkySatCollect
+    """
+    with rasterio.open(raster_path, "r") as src:
+        updated_meta = src.profile.copy()
+        with rasterio.open(new_raster_path, "w", **updated_meta) as dst:
+            dst.update_tags(start_date=date, end_date=date, request_id=request_id)
+            for band in range(1, 4):
+                cur_band = src.read(band)
+                dst.write(cur_band, band)
+
+
+def merge_rasters(raster_path_list, date, save_path, product):
     raster_new_list = []
     for raster_path in raster_path_list:
         new_raster_path_tmp = raster_path.replace(".tif", "_converted_tmp.tif")
         raster_new_list.append(new_raster_path_tmp)
-        transform_raster(raster_path, new_raster_path_tmp, date)
+        if product == 'SkySatCollect':
+            transform_raster_visual(raster_path, new_raster_path_tmp, date)
+        else:
+            transform_raster(raster_path, new_raster_path_tmp, date)
 
     listToStr = ' '.join([str(elem) for elem in raster_new_list])
     subprocess.call(' '.join(["gdalwarp --config GDAL_CACHEMAX 3000 -wm 3000 -t_srs EPSG:3857", listToStr, save_path]),
@@ -60,7 +76,7 @@ if __name__ == "__main__":
                         help="""path to save merged raster""")
 
     path_dict = {'PSScene4Band': "files/PSScene4Band/*/analytic_sr_udm2/*_SR.tif",
-                 'SkySatCollect': "files/SkySatCollect/*/pansharpened_udm2/*_pansharpened.tif"}
+                 'SkySatCollect': "files/SkySatCollect/*/*_visual.tif"}
     args = parser.parse_args()
     raster_list = glob(path_dict.get(args.product))
-    merge_rasters(raster_list, args.date, args.save_path)
+    merge_rasters(raster_list, args.date, args.save_path, args.product)
