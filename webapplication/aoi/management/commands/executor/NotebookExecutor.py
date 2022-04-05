6ec37e46ac00d65e1a208f9dbc44b33250a26014
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2022-04-05 20:26:01 +0300

    19055 added additional parameters to NotebookExecutor

--- webapplication/aoi/management/commands/executor/NotebookExecutor.py
+++ webapplication/aoi/management/commands/executor/NotebookExecutor.py
@@ -18,6 +18,7 @@ parser.add_argument('--kernel', type=str, help='Kernel name', default=None)
 parser.add_argument('--cell_timeout', type=int, help='Max execution time (sec) for cell', required=True)
 parser.add_argument('--notebook_timeout', type=int, help='Max execution time (sec) for full notebook process',
                     required=True)
+parser.add_argument('--parameters', type=str, help='Additional parameters', default=None)
 
 logger = logging.getLogger(__name__)
 
@@ -31,6 +32,12 @@ class NotebookExecutor:
                            AOI=args.aoi,
                            START_DATE=args.start_date if args.start_date != 'None' else None,
                            END_DATE=args.end_date if args.end_date != 'None' else None)
+
+        if args.parameters:
+            additional_parameters = json.loads(args.parameters)
+            for parameter in additional_parameters:
+                parameter_upper = parameter.upper()
+                self.PARAMS[parameter_upper] = additional_parameters[parameter]
 
         self.cell_timeout = args.cell_timeout
         self.notebook_timeout = args.notebook_timeout
