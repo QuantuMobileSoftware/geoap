294186378fad2e17ac534e5e8a46bb5d6a45f2f8
DmitriyLugovoy <dmitriy.lugovoy.79@gmail.com>
2022-04-20 20:17:03 +0300

    19080 updated constants with planet bands in products information

--- data/notebooks/planet_downloader/planet_processing/constants.py
+++ data/notebooks/planet_downloader/planet_processing/constants.py
@@ -1,14 +1,65 @@
+
+# <acquisition date>_<acquisition time>_<satellite_id>_<productLevel><bandProduct>.<extension>
+
+s = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'UTM'}
+ps_basic_scene_product_3band = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': None}
+ps_basic_scene_product_4band = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': None}
+
+ps_basic_analytic_scene_product_3band = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'WGS84'}
+ps_basic_analytic_scene_product_4band = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'WGS84'}
+ps_ortho_scene_product_3band = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+ps_ortho_scene_product_4band = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'UTM'}
+ps_ortho_scene_product_8band = {'bands': {
+    'Coastal_Blue': 1, 'B': 2, 'G_I': 3, 'G': 4, 'Yellow': 5, 'R': 6, 'Red-Edge': 7, 'NIR': 8}, 'projection': 'UTM'
+}
+
+ps_visual_ortho_tile_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+ps_analytic_ortho_tile_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+ps_analytic_sr_ortho_tile_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+ps_visual_ortho_scene_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+ps_analytic_ortho_scene_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+ps_analytic_sr_ortho_scene_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+
+skysat_basic_panchromatic_scene_product = {'bands': {'PAN': 1}, 'projection': 'WGS84'}
+skysat_basic_analytic_scene_product = {'bands': {'B': 1, 'G': 2, 'R': 3, 'PAN': 4, 'NIR': 5}, 'projection': 'WGS84'}
+
+skysat_panchromatic_ortho_scene_product = {'bands': {'PAN': 1}, 'projection': 'UTM'}
+skysat_panchromatic_ortho_collect_product = {'bands': {'PAN': 1}, 'projection': 'UTM'}
+
+skysat_visual_ortho_scene_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+skysat_visual_ortho_collect_product = {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}
+
+skysat_pansharpened_ortho_scene_product = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'UTM'}
+skysat_pansharpened_ortho_collect_product = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'UTM'}
+
+skysat_analytic_sr_ortho_scene_product = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'UTM'}
+skysat_analytic_ortho_scene_product = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'UTM'}
+skysat_analytic_ortho_collect_product = {'bands': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4}, 'projection': 'UTM'}
+
 assets_in_bundles_for_visualizing = dict(
-    analytic_8b_sr_udm2={'PSScene': 'ortho_analytic_8b_sr'},
-    pansharpened_udm2={'SkySatCollect': 'ortho_pansharpened', 'SkySatScene': 'ortho_pansharpened'},
+    analytic_8b_sr_udm2={'PSScene': {'file_name': 'ortho_analytic_8b_sr', 'properties': ps_ortho_scene_product_8band}},
+    pansharpened_udm2={
+        'SkySatCollect': {'file_name': 'ortho_pansharpened', 'properties': skysat_pansharpened_ortho_collect_product},
+        'SkySatScene': {'file_name': 'ortho_pansharpened', 'properties': skysat_pansharpened_ortho_scene_product}
+    },
     analytic_sr_udm2={
-        'PSScene4Band': 'analytic_sr',
-        'PSScene': 'ortho_analytic_4b_sr',
-        'PSOrthoTile': 'analytic_sr',
-        'SkySatCollect': 'ortho_analytic_sr',
-        'SkySatScene': 'ortho_analytic_sr'
-    }
+        'PSScene4Band': {'file_name': 'analytic_sr', 'properties': ps_ortho_scene_product_4band},
+        'PSScene': {'file_name': 'ortho_analytic_4b_sr', 'properties': ps_ortho_scene_product_4band},
+        'PSOrthoTile': {'file_name': 'analytic_sr', 'properties': ps_analytic_sr_ortho_tile_product},
+        'SkySatCollect': {'file_name': 'ortho_analytic_sr', 'properties': skysat_analytic_ortho_collect_product},
+        'SkySatScene': {'file_name': 'ortho_analytic_sr', 'properties': skysat_analytic_ortho_scene_product}
+    }, 
+    visual={
+        'PSOrthoTile': {'file_name': 'visual', 'properties': ps_visual_ortho_tile_product},
+        'PSScene3Band': {'file_name': 'visual', 'properties': ps_ortho_scene_product_3band},
+        'PSScene': {'file_name': 'ortho_visual', 'properties': ps_visual_ortho_scene_product},
+        'REOrthoTile': {'file_name': 'visual', 'properties': None},
+        'Sentinel2L1C': {'file_name': 'visual', 'properties': None},
+        'SkySatCollect': {'file_name': 'ortho_visual', 'properties': skysat_visual_ortho_collect_product},
+        'SkySatScene': {'file_name': 'ortho_visual', 'properties': skysat_visual_ortho_scene_product}}
+    
 )
+
 
 ps_bands_order_for_visualizing = {
     'PS2': [3, 2, 1],
@@ -18,13 +69,11 @@ ps_bands_order_for_visualizing = {
 skysat_bands_order_for_visualizing = {'skysat': [3, 2, 1]}
 
 planetscope_bands = {
-    'PS2': {1: 'Blue', 2: 'Green', 3: 'Red', 4: 'NIR'},
-    'PS2.SD': {1: 'Blue', 2: 'Green', 3: 'Red', 4: 'NIR'},
-    'PSB.SD': {
-        1: 'Coastal_Blue', 2: 'Blue', 3: 'Green_I', 4: 'Green_II', 5: 'Yellow', 6: 'Red', 7: 'Red-Edge', 8: 'NIR',
-    }
+    'PS2': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4},
+    'PS2.SD': {'B': 1, 'G': 2, 'R': 3, 'NIR': 4},
+    'PSB.SD': {'Coastal_Blue': 1, 'B': 2, 'G_I': 3, 'G': 4, 'Yellow': 5, 'R': 6, 'Red-Edge': 7, 'NIR': 8}
 }
-skysat_bands = {1: 'Blue', 2: 'Green', 3: 'Red', 4: 'NIR', 5: 'Pan'}
+skysat_bands = {'B': 1, 'G': 2, 'R': 3, 'NIR': 4, 'Pan': 5}
 
 planetscope_item_types = {
     'PSScene': {
--- data/notebooks/planet_downloader/planet_processing/processor.py
+++ data/notebooks/planet_downloader/planet_processing/processor.py
@@ -4,14 +4,14 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import rasterio
+from rasterio.windows import Window
 import os
 import shutil
 from subprocess import Popen, PIPE, TimeoutExpired
 from tempfile import TemporaryDirectory
 import time
 import zipfile
-from .constants import assets_in_bundles_for_visualizing, ps_bands_order_for_visualizing, \
-    skysat_bands_order_for_visualizing, planetscope_item_types, skysat_item_types, planetscope_bands
+from .constants import assets_in_bundles_for_visualizing
 from .utils import transform_crs
 from .utils import scale_band, min_max_scale
 
@@ -264,8 +264,8 @@ class PlanetBase:
         :return: Dict: bands dict where keys can be B', 'G', 'R', 'NIR', 'Red-Edge', 'Yellow', 'G_I', 'Coastal_Blue'
         """
         return assets_in_bundles_for_visualizing[product_bundle][item_type]['properties']['bands']
-
-
+    
+        
 class PlanetVisualizer(PlanetBase):
     """
     Class for extracting and converting files from order archive for further visualization using 'gdal2tiles.py'
@@ -281,32 +281,32 @@ class PlanetVisualizer(PlanetBase):
         self.output_dir_path = output_dir_path
         self.delete_temp = delete_temp
     
-    def __check_bundle_type(self, row, product_bundle, item_type):
+    @staticmethod
+    def get_file_properties(product_bundle, item_type):
         """
-        Check if product_bundle in known bundles for visualizing,
+        Check if product_bundle and item_type are in known bundles for visualizing,
         if it is not, then raise KeyError,
-        else check if asset_type from row equal to asset_type from assets_in_bundles_for_visualizing dict.
-        :param row: Pandas DatFrame row
+        else return dict with file name for extracting from archive and product properties
         :param product_bundle: str: product bundle name
         :param item_type: str: product item type
-        :return: Bool
+        :return: dict: {'file_name': 'ortho_visual', 'properties':
+        {'bands': {'R': 1, 'G': 2, 'B': 3}, 'projection': 'UTM'}}
         """
         if product_bundle not in assets_in_bundles_for_visualizing.keys():
-            print('Unknown planet/bundle_type!', row['planet/bundle_type'])
+            print('Unknown planet/bundle_type!', product_bundle)
             raise KeyError
         if item_type not in assets_in_bundles_for_visualizing[product_bundle].keys():
-            print('Unknown planet/asset_type! in assets_in_bundles_for_visualizing', row['planet/asset_type'])
+            print('Unknown planet/asset_type! in assets_in_bundles_for_visualizing', item_type)
             raise KeyError
-        return row['planet/asset_type'] == assets_in_bundles_for_visualizing[product_bundle][item_type]['file_name']
+        return assets_in_bundles_for_visualizing[product_bundle][item_type]
     
     @staticmethod
     def filter_product_by_item_id(row, item_id):
         return row['planet/item_id'] == item_id
-    
-    def filter_product_by_product_bundle(self, products_df, product_bundle, item_type):
-        return products_df[products_df.annotations.apply(lambda row: self.__check_bundle_type(
-            row, product_bundle, item_type
-        ))]
+        
+    @staticmethod
+    def filter_product_by_bundle_assets_file_name(products_df, file_name):
+        return products_df[products_df.annotations.apply(lambda row: row['planet/asset_type'] == file_name)]
     
     def get_bands_order_for_visualization(self, product_bundle, item_type):
         """
@@ -318,10 +318,62 @@ class PlanetVisualizer(PlanetBase):
         bands_to_extract = ['R', 'G', 'B']
         product_bands_order = self.get_product_bands_order(product_bundle, item_type)
         return [product_bands_order[band] for band in bands_to_extract]
+
+    @staticmethod
+    def create_reordered_image(raster_path, bands_order, step=6000):
+        """
+        Create new image with bands dtype='uint8' and std normalization
+        :param raster_path: Path: Path to source image
+        :param bands_order: list or tuple
+        :param step: step of window normalization
+        :return: Path: Path to destination image
+        """
+        raster_dst = raster_path.parent / f'{raster_path.stem}_rgb{raster_path.suffix}'
+        if raster_dst.with_suffix('.json').exists():
+            return raster_dst
+        src = rasterio.open(raster_path)
+        w, h = src.meta['width'], src.meta['height']
+        whole_rem_w = divmod(w, step)
+        whole_rem_h = divmod(h, step)
+    
+        whole_i_h_steps = [(0, i * step, 0, step) for i in range(whole_rem_h[0])]
+        all_steps_h = whole_i_h_steps + [(0, whole_i_h_steps[-1][1] + step, 0, whole_rem_h[1])]
+    
+        all_steps = []
+        for h_step in all_steps_h:
+            all_steps = all_steps + [(i * step, h_step[1], step, h_step[-1]) for i in range(whole_rem_w[0])]
+            all_steps = all_steps + [(all_steps[-1][0] + step, h_step[1], whole_rem_w[1], h_step[-1])]
+    
+        pixels_sum = np.sum([np.sum(src.read((1, 2, 3), window=Window(*i)), axis=(1, 2)) for i in all_steps], axis=0)
+    
+        means_channels = (pixels_sum / (w * h)).reshape((3, 1, 1))
+    
+        squared_deviation = np.sum(
+            [np.sum((src.read((1, 2, 3), window=Window(*i)) - means_channels) ** 2, axis=(1, 2)) for i in all_steps],
+            axis=0)
+    
+        std = (squared_deviation / (w * h)) ** 0.5
+    
+        max_ = (means_channels.reshape(3, -1) + 2 * std.reshape(3, -1)).reshape(3, 1, 1)
+    
+        profile = src.profile
+        profile['dtype'] = 'uint8'
+        profile['count'] = 3
+    
+        with rasterio.open(
+                raster_dst, 'w', **profile
+        ) as dst:
+            for i in all_steps:
+                window_normalize = src.read((1, 2, 3), window=Window(*i)) / max_
+                window_normalize = np.clip(window_normalize * 255, 0, 255).astype(rasterio.uint8)
+                dst.write(window_normalize, window=Window(*i))
+    
+        return raster_dst
         
     @staticmethod
-    def create_reordered_image(raster_path, bands_order):
+    def create_reordered_image_v1(raster_path, bands_order):
         """
+        TODO rm this
         Create new image with bands dtype='uint8' ordered according to bands_order
         :param raster_path: Path: Path to source image
         :param bands_order: list or tuple
@@ -334,31 +386,9 @@ class PlanetVisualizer(PlanetBase):
             updated_meta = src.profile.copy()
             updated_meta['count'] = len(bands_order)
             updated_meta['bands'] = len(bands_order)
-            # updated_meta.update({
-            #     "nodata": 0,
-            #     "dtype": 'uint8'
-            # })
             with rasterio.open(raster_dst, 'w', **updated_meta) as dst:
                 for num, band_num in enumerate(bands_order, start=1):
                     band = src.read(band_num)
-                    # max_ = np.percentile(band, 98)
-                    # min_ = np.percentile(band, 2)
-                    # band[band > max_] = max_
-                    # band[band < min_] = min_
-                    # band = (band - min_) / (max_ - min_) * 255
-
-                    # arr = np.nonzero(band)
-                    # max_ = np.percentile(arr, 98)
-                    # min_ = np.nanpercentile(arr, 2)
-                    # max_ = np.percentile(band, 98)
-                    # min_ = np.percentile(band, 2)
-                    # band[band > max_] = max_
-                    # band = np.where((band > 0) & (band < min_), min_, band)
-                    # band[band > 0] = (band - min_) / (max_ - min_) * 254 + 1
-                    
-                    # band = min_max_scale(band)
-                    # band = band.astype(np.uint8)
-                    
                     dst.write(band, indexes=num)
         return raster_dst
 
@@ -380,38 +410,9 @@ class PlanetVisualizer(PlanetBase):
         self.run_process(command, self.max_timeout_for_img_generation)
         return raster_dst
     
-    def planetscope_processor(self, product):
+    def product_filter(self, product):
         """
-        Process planetscope ordered images
-        :param product: obj:
-        :return: pandas DataFrame: DataFrame with column names: 'path', 'size', 'item_id', 'bands_order'
-        """
-        img_df = self.get_empty_img_df()
-        for item_id in product['item_ids']:
-            item_info = self.get_item_info(item_id, product['item_type'])
-            instrument = item_info['properties']['instrument']
-            if instrument not in planetscope_bands.keys():
-                print(f'Unknown instrument "{instrument}" in planetscope_bands! Skipping item {item_info["id"]}')
-                print(f'path to skipped file {item_info["path"]}')
-                continue
-            item_to_process = self.filtered_products_df[self.filtered_products_df.annotations.apply(
-                lambda row: self.filter_product_by_item_id(row, item_id)
-            )]
-            item_path_in_archive = item_to_process['path'].values[0]
-            item_size = item_to_process['size'].values[0]
-            bands_order = ps_bands_order_for_visualizing[instrument]
-            
-            # add new row to img_df
-            img_df.loc[img_df.shape[0]] = [item_path_in_archive, item_size, item_id, bands_order, ]
-            
-        self.extract_img_files_from_archiwe(img_df)
-        
-        img_converted_list = self.reorder_item_bands(img_df, self.create_reordered_image)
-        return img_converted_list
-        
-    def skysat_processor(self, product):
-        """
-        Process Skysat ordered images
+        Filter ordered images
         :param product: obj:
         :return: pandas DataFrame: DataFrame with column names: 'path', 'size', 'item_id', 'bands_order'
         """
@@ -426,7 +427,7 @@ class PlanetVisualizer(PlanetBase):
             item_path_in_archive = item_to_process['path'].values[0]
             item_size = item_to_process['size'].values[0]
             bands_order = self.get_bands_order_for_visualization(product_bundle, item_type)
-
+            
             # add new row to img_df
             img_df.loc[img_df.shape[0]] = [item_path_in_archive, item_size, item_id, bands_order, ]
         return img_df
@@ -436,16 +437,15 @@ class PlanetVisualizer(PlanetBase):
             self.img_converted_list = []
             item_type = product['item_type']
             product_bundle = product['product_bundle']
-            self.filtered_products_df = self.filter_product_by_product_bundle(
-                self.found_products_df, product_bundle, item_type)
-            if item_type in planetscope_item_types.keys():
-                self.img_df = self.planetscope_processor(product)
+            file_properties = self.get_file_properties(product_bundle, item_type)
 
-            if item_type in skysat_item_types.keys():
-                self.img_df = self.skysat_processor(product)
+            self.filtered_products_df = self.filter_product_by_bundle_assets_file_name(
+                self.found_products_df, file_properties['file_name']
+            )
+
+            self.img_df = self.product_filter(product)
                 
             self.extract_img_files_from_archiwe(self.img_df)
-            # self.img_converted_list = self.reorder_item_bands(self.img_df, self.create_reordered_image)
             self.img_converted_list = self.reorder_item_bands(self.img_df, self.create_reordered_image)
             self.img_transformed_list = self.transform_img(self.img_converted_list)
 
