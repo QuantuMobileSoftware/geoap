48bc1023eb0eb026948c3d1463622e5405475c1b
adolgarev <a.dolgarev@quantumobile.com>
2022-05-03 17:06:49 +0300

    19080: fix BASE_CONTAINER_NAME

--- webapplication/sip/settings-ukraine.py
+++ webapplication/sip/settings-ukraine.py
@@ -8,7 +8,7 @@ CSRF_TRUSTED_ORIGINS = ["soilmateukraine.quantumobile.co"]
 
 # AOI app, notebook_executor
 NOTEBOOK_EXECUTOR_GPUS = "1"
-BASE_CONTAINER_NAME = "sip_code_webapplication_1"
+BASE_CONTAINER_NAME = "sip_webapplication_1"
 HOST_VOLUME_DATA_BASENAME = "sip"
 
 MAX_TIMEOUT_FOR_TILE_CREATION_SECONDS = 60 * 60 * 1 * 60
