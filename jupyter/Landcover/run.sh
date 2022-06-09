80087439e64d7ffec347eecb40e71c4475c10969
erelin6613 <valentineburn@gmail.com>
2022-06-09 19:05:43 +0300

    Corrected port

--- jupyter/Landcover/run.sh
+++ jupyter/Landcover/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -f Dockerfile -t landcover .
-docker run -d -p 8847:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name landcover_1 landcover:latest start-notebook.sh --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$xjV2VuXfOdrjmqByCU29gA$+k3SBfXKBPni/nmkFrZvVA'
+docker run -d -p 5432:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name landcover_1 landcover:latest start-notebook.sh --NotebookApp.password='argon2:$argon2id$v=19$m=10240,t=10,p=8$xjV2VuXfOdrjmqByCU29gA$+k3SBfXKBPni/nmkFrZvVA'
