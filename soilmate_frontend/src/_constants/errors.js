a0d60116770a0eb1e29452488664dcd5769b9bb0
Stanislav <35593946+StanislavKharkiv@users.noreply.github.com>
2022-06-29 12:09:05 +0300

    19818/fe/start end date & login error (#293)
    * 19818/hide date range if not required
    
    * 17771/fix login error

--- soilmate_frontend/src/_constants/errors.js
+++ soilmate_frontend/src/_constants/errors.js
@@ -3,3 +3,4 @@ export const SERVER_ERROR = "Sorry, we've got a server error, please try a bit l
 export const FILE_ERROR =
   'Sorry, we can not process your file. Please check its size/format';
 export const SIZE_ERROR = 'To access more areas, please contact the administrator';
+export const DEFAULT_ERROR = 'Something went wrong';
