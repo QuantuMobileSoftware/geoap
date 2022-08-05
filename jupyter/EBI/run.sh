036a28f1faabdc4203eddcdaf3731da6ad8dbe40
latushkinams <margarita.latushkina@oxagile.com>
2022-08-05 14:38:27 +0300

    fix repository name

--- jupyter/EBI/run.sh
+++ jupyter/EBI/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker build -f Dockerfile -t EBI_jupyter .
-docker run -d -p 8845:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name EBI_jupyter_1 EBI_jupyter:latest start-notebook.sh --NotebookApp.password='54fc29cc3359e4689a37e40601bb957e'
+docker build -f Dockerfile -t ebi_jupyter .
+docker run -d -p 8845:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name ebi_jupyter_1 ebi_jupyter:latest start-notebook.sh --NotebookApp.password='54fc29cc3359e4689a37e40601bb957e'
