d26c17059b63bdbce8b540dded51ca8a2c38b99f
latushkinams <margarita.latushkina@oxagile.com>
2022-08-05 13:44:28 +0300

    change port and password

--- jupyter/EBI/run.sh
+++ jupyter/EBI/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -f Dockerfile -t EBI_jupyter .
-docker run -d -p 8847:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name EBI_jupyter_1 EBI_jupyter:latest start-notebook.sh --NotebookApp.password='sha1:3a4d2020156f:2c3aa52c72a8cbbac85734e255c340510eb75eac'
+docker run -d -p 8845:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name EBI_jupyter_1 EBI_jupyter:latest start-notebook.sh --NotebookApp.password='54fc29cc3359e4689a37e40601bb957e'
