118eb9251f7f2a2c64b2421ddaaacde4084e1cdc
Dmytro Lukash <dmitri.lukash@gmail.com>
2022-10-20 16:35:55 +0300

    20876/upd spartup script

--- k8s/helpers/start_cluster.sh
+++ k8s/helpers/start_cluster.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
+# Creating cluster
 kind create cluster --config=./k8s/helpers/kind-config.yaml
 kind load docker-image registry.quantumobile.co/sip-web-server:latest registry.quantumobile.co/sip-web-application:latest
+
 
 kubectl apply -k ./k8s/
 
@@ -16,4 +18,12 @@ kubectl apply -f ./k8s/webapplication/web-application-service.yaml
 kubectl wait deployment -n prod webapplication --for condition=Available=True
 
 kubectl apply -f ./k8s/webserver/webserver-deployment.yaml
-kubectl apply -f ./k8s/webserver/webserver-service-nodeport.yaml
+kubectl apply -f ./k8s/webserver/webserver-service.yaml
+
+# Ingres and controller
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
+
+kubectl wait deployment -n projectcontour contour --for condition=Available=True
+kubectl patch daemonsets -n projectcontour envoy -p '{"spec":{"template":{"spec":{"nodeSelector":{"ingress-ready":"true"},"tolerations":[{"key":"node-role.kubernetes.io/control-plane","operator":"Equal","effect":"NoSchedule"},{"key":"node-role.kubernetes.io/master","operator":"Equal","effect":"NoSchedule"}]}}}}'
+
+kubectl apply -f ./k8s/sip-ingres.yaml
