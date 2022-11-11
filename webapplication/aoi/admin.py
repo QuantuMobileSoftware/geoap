54bf7e35401f070c984b5661524501cc7136380c
adolgarev <a.dolgarev@quantumobile.com>
2022-11-11 19:18:20 +0200

    301: formatting

--- webapplication/aoi/admin.py
+++ webapplication/aoi/admin.py
@@ -1,67 +1,126 @@
 from django.contrib.gis import admin
 from .forms import ComponentAdminForm
-from .models import (
-    AoI,
-    JupyterNotebook,
-    Request,
-    Component,
-    Input,
-    Output
-)  
+from .models import AoI, JupyterNotebook, Request, Component, Input, Output
 from django.db.models import JSONField
 from flat_json_widget.widgets import FlatJsonWidget
 
 
 @admin.register(AoI)
 class AoIAdmin(admin.OSMGeoAdmin):
-    list_display = ('user', 'name', 'polygon', 'createdat', 'type')
-    search_fields = ('name', 'type')
-    fieldsets = (('fieldsets_name', {
-        'fields': ('user', 'name', 'polygon', 'type')
-    }),)
+    list_display = ("user", "name", "polygon", "createdat", "type")
+    search_fields = ("name", "type")
+    fieldsets = (("fieldsets_name", {"fields": ("user", "name", "polygon", "type")}),)
 
 
 @admin.register(JupyterNotebook)
 class JupyterNotebookAdmin(admin.OSMGeoAdmin):
-    list_display = ('pk', 'name', 'image', 'path', 'kernel_name', 'run_validation', 'success', 'options',
-                    'additional_parameter', 'period_required',)
-    search_fields = ('name', 'image', 'path', 'kernel_name', 'run_validation', 'success', )
-    readonly_fields = ('pk', 'run_validation', 'success', )
+    list_display = (
+        "pk",
+        "name",
+        "image",
+        "path",
+        "kernel_name",
+        "run_validation",
+        "success",
+        "options",
+        "additional_parameter",
+        "period_required",
+    )
+    search_fields = (
+        "name",
+        "image",
+        "path",
+        "kernel_name",
+        "run_validation",
+        "success",
+    )
+    readonly_fields = (
+        "pk",
+        "run_validation",
+        "success",
+    )
 
     formfield_overrides = {
         JSONField: {
-            'widget': FlatJsonWidget,
+            "widget": FlatJsonWidget,
         },
     }
 
 
 @admin.register(Request)
 class RequestAdmin(admin.OSMGeoAdmin):
-    list_display = ('pk', 'user', 'aoi', 'notebook', 'date_from', 'date_to', 'started_at', 'finished_at',
-                    'calculated', 'success', 'error', 'additional_parameter')
-    readonly_fields = ['pk', 'started_at', 'calculated', 'error', ]
+    list_display = (
+        "pk",
+        "user",
+        "aoi",
+        "notebook",
+        "date_from",
+        "date_to",
+        "started_at",
+        "finished_at",
+        "calculated",
+        "success",
+        "error",
+        "additional_parameter",
+    )
+    readonly_fields = [
+        "pk",
+        "started_at",
+        "calculated",
+        "error",
+    ]
 
 
 @admin.register(Component)
 class ComponentAdmin(admin.OSMGeoAdmin):
     form = ComponentAdminForm
-    list_display = ('pk', 'name', 'image', 'command', 'require_GPU', 'run_validation', 'success')
-    search_fields = ('name', 'image', )
-    readonly_fields = ('pk', 'run_validation', 'success', )
-    list_filter = ('require_GPU', 'run_validation', 'success', 'inputs__name', )
+    list_display = (
+        "pk",
+        "name",
+        "image",
+        "command",
+        "require_GPU",
+        "run_validation",
+        "success",
+    )
+    search_fields = (
+        "name",
+        "image",
+    )
+    readonly_fields = (
+        "pk",
+        "run_validation",
+        "success",
+    )
+    list_filter = (
+        "require_GPU",
+        "run_validation",
+        "success",
+        "inputs__name",
+    )
 
 
 @admin.register(Input)
 class InputAdmin(admin.OSMGeoAdmin):
-    list_display = ('pk', 'name', 'type', 'assigned_from', 'is_file',)
-    search_fields = ('name', )
-    readonly_fields = ('pk',)
-    list_filter = ('type', 'assigned_from','is_file', )
+    list_display = (
+        "pk",
+        "name",
+        "type",
+        "assigned_from",
+        "is_file",
+    )
+    search_fields = ("name",)
+    readonly_fields = ("pk",)
+    list_filter = (
+        "type",
+        "assigned_from",
+        "is_file",
+    )
+
 
 @admin.register(Output)
 class OutputAdmin(admin.OSMGeoAdmin):
-    list_display=('pk', 'name', 'is_error')
-    search_fields = ('name', )
-    readonly_fields = ('pk',)
-    list_filter = ('is_error', )
-
+    list_display = ("pk", "name", "is_error")
+    search_fields = ("name",)
+    readonly_fields = ("pk",)
+    list_filter = ("is_error",)
--- webapplication/aoi/models.py
+++ webapplication/aoi/models.py
@@ -7,14 +7,18 @@ from django.utils import timezone
 class AoI(models.Model):
     USER_DEFINED_TYPE = 1
     FIELD_TYPE = 2
-    AREA_TYPE_CHOICES = (
-        (USER_DEFINED_TYPE, 'AREA'),
-        (FIELD_TYPE, 'FIELD')
+    AREA_TYPE_CHOICES = ((USER_DEFINED_TYPE, "AREA"), (FIELD_TYPE, "FIELD"))
+
+    user = models.ForeignKey(
+        "user.User",
+        on_delete=models.CASCADE,
+        verbose_name="User id",
+        related_name="aoi",
     )
-    
-    user = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name='User id', related_name='aoi')
-    name = models.CharField(max_length=200, blank=False, null=False, unique=True, verbose_name='AOI name')
-    polygon = models.PolygonField(spatial_index=True, verbose_name='Polygon')
+    name = models.CharField(
+        max_length=200, blank=False, null=False, unique=True, verbose_name="AOI name"
+    )
+    polygon = models.PolygonField(spatial_index=True, verbose_name="Polygon")
     createdat = models.DateTimeField(default=timezone.now)
     type = models.IntegerField(choices=AREA_TYPE_CHOICES, default=USER_DEFINED_TYPE)
 
@@ -22,106 +26,134 @@ class AoI(models.Model):
         return self.name
 
     class Meta:
-        verbose_name = 'Area of interest'
-        verbose_name_plural = 'Areas of interest'
-        ordering = ['name']
-        
-        
+        verbose_name = "Area of interest"
+        verbose_name_plural = "Areas of interest"
+        ordering = ["name"]
+
+
 class JupyterNotebook(models.Model):
-    name = models.CharField(max_length=200, blank=False, null=False, unique=True, verbose_name='Notebook name')
-    image = models.CharField(max_length=400, verbose_name='Image')
-    path = models.CharField(max_length=200, unique=True, verbose_name='Path to a notebook')
-    kernel_name = models.CharField(max_length=200, null=False, blank=False, verbose_name='Kernel name')
-    options = JSONField(blank=True, null=True, verbose_name='Additional container options')
-    run_validation = models.BooleanField(default=False, verbose_name='Run validation')
-    success = models.BooleanField(default=False, verbose_name='Validation succeeded')
-    additional_parameter = models.CharField(max_length=50, null=True, blank=True, verbose_name='Additional parameter')
-    run_on_gpu = models.BooleanField(default=True, verbose_name='Whether GPU is needed for a notebook to run')
-    period_required = models.BooleanField(default=True, verbose_name='Start and end dates are required')
+    name = models.CharField(
+        max_length=200,
+        blank=False,
+        null=False,
+        unique=True,
+        verbose_name="Notebook name",
+    )
+    image = models.CharField(max_length=400, verbose_name="Image")
+    path = models.CharField(
+        max_length=200, unique=True, verbose_name="Path to a notebook"
+    )
+    kernel_name = models.CharField(
+        max_length=200, null=False, blank=False, verbose_name="Kernel name"
+    )
+    options = JSONField(
+        blank=True, null=True, verbose_name="Additional container options"
+    )
+    run_validation = models.BooleanField(default=False, verbose_name="Run validation")
+    success = models.BooleanField(default=False, verbose_name="Validation succeeded")
+    additional_parameter = models.CharField(
+        max_length=50, null=True, blank=True, verbose_name="Additional parameter"
+    )
+    run_on_gpu = models.BooleanField(
+        default=True, verbose_name="Whether GPU is needed for a notebook to run"
+    )
+    period_required = models.BooleanField(
+        default=True, verbose_name="Start and end dates are required"
+    )
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Jupyter Notebook'
-        verbose_name_plural = 'Jupyter Notebooks'
-        ordering = ['name']
-       
+        verbose_name = "Jupyter Notebook"
+        verbose_name_plural = "Jupyter Notebooks"
+        ordering = ["name"]
+
 
 class Input(models.Model):
-
     class InputDataType(models.TextChoices):
-        INT = 'INT','Integer'
-        STR = 'STR','String'
-        FLOAT = 'FLOAT','Float'
-        BOOL = 'BOOL','Boolean'
-        LIST = 'LIST','List'
-        DATE = 'DATE','Date'
+        INT = "INT", "Integer"
+        STR = "STR", "String"
+        FLOAT = "FLOAT", "Float"
+        BOOL = "BOOL", "Boolean"
+        LIST = "LIST", "List"
+        DATE = "DATE", "Date"
 
     class AssigningMethod(models.TextChoices):
-        GLOBAL_SETTINGS = "settings",'Global settings'
-        USER_ATTRIBUTES = "user",'User attributes'
-        REQUEST_ATTRIBUTES = "request",'Request attributes'
-        ANOTHER_COMPONENT = "component",'Another component'
+        GLOBAL_SETTINGS = "settings", "Global settings"
+        USER_ATTRIBUTES = "user", "User attributes"
+        REQUEST_ATTRIBUTES = "request", "Request attributes"
+        ANOTHER_COMPONENT = "component", "Another component"
 
-    name = models.CharField(max_length=50, verbose_name='Name')
+    name = models.CharField(max_length=50, verbose_name="Name")
     type = models.CharField(
-        max_length=10, 
-        verbose_name='Type',
+        max_length=10,
+        verbose_name="Type",
         choices=InputDataType.choices,
-        default=InputDataType.STR
+        default=InputDataType.STR,
     )
-    assigned_from = models.CharField(max_length=20,
+    assigned_from = models.CharField(
+        max_length=20,
         choices=AssigningMethod.choices,
         default=AssigningMethod.REQUEST_ATTRIBUTES,
-        verbose_name="Attribute's origin"
+        verbose_name="Attribute's origin",
     )
-    is_file = models.BooleanField(default=False, verbose_name='Is it a file?')
+    is_file = models.BooleanField(default=False, verbose_name="Is it a file?")
 
     def __str__(self) -> str:
         return f'{self.name} | {self.type} | From: "{self.assigned_from}"'
 
 
 class Output(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Name')
-    is_error = models.BooleanField(default=False, verbose_name='Is error output?')
-    
+    name = models.CharField(max_length=50, verbose_name="Name")
+    is_error = models.BooleanField(default=False, verbose_name="Is error output?")
+
 
 class Component(models.Model):
-    name = models.CharField(max_length=50, verbose_name='Name')
-    image = models.CharField(max_length=255, verbose_name='Image tag')
+    name = models.CharField(max_length=50, verbose_name="Name")
+    image = models.CharField(max_length=255, verbose_name="Image tag")
     command = models.CharField(
-        max_length=1000,
-        null=True, 
-        blank=True,
-        verbose_name='Command'
+        max_length=1000, null=True, blank=True, verbose_name="Command"
     )
     inputs = models.ManyToManyField(Input)
     outputs = models.ManyToManyField(Output)
-    require_GPU = models.BooleanField(default=False, verbose_name='Whether GPU is needed for a component to run')
-    run_validation = models.BooleanField(default=False, verbose_name='Run validation')
-    success = models.BooleanField(default=False, verbose_name='Validation succeeded')
+    require_GPU = models.BooleanField(
+        default=False, verbose_name="Whether GPU is needed for a component to run"
+    )
+    run_validation = models.BooleanField(default=False, verbose_name="Run validation")
+    success = models.BooleanField(default=False, verbose_name="Validation succeeded")
 
     def __str__(self) -> str:
         return self.name
 
 
 class Request(models.Model):
-    user = models.ForeignKey('user.User', on_delete=models.PROTECT, verbose_name='User id')
-    aoi = models.ForeignKey(AoI, null=True, on_delete=models.SET_NULL, verbose_name='AOI id')
-    notebook = models.ForeignKey(
-        JupyterNotebook, on_delete=models.PROTECT,
-        verbose_name='Notebook id',
+    user = models.ForeignKey(
+        "user.User", on_delete=models.PROTECT, verbose_name="User id"
     )
-    date_from = models.DateField(blank=True, null=True, verbose_name='Date from')
-    date_to = models.DateField(blank=True, null=True, verbose_name='Date to')
-    started_at = models.DateTimeField(blank=True, null=True, verbose_name='Started at')
-    finished_at = models.DateTimeField(blank=True, null=True, verbose_name='Finished at')
-    calculated = models.BooleanField(default=False, verbose_name='Notebook calculated')
-    success = models.BooleanField(default=False, verbose_name='Execution succeeded')
-    error = models.CharField(max_length=400, blank=True, null=True, verbose_name='Error')
-    polygon = models.PolygonField(spatial_index=True, verbose_name='Polygon')
-    additional_parameter = models.CharField(max_length=50, null=True, blank=True, verbose_name='Additional parameter')
+    aoi = models.ForeignKey(
+        AoI, null=True, on_delete=models.SET_NULL, verbose_name="AOI id"
+    )
+    notebook = models.ForeignKey(
+        JupyterNotebook,
+        on_delete=models.PROTECT,
+        verbose_name="Notebook id",
+    )
+    date_from = models.DateField(blank=True, null=True, verbose_name="Date from")
+    date_to = models.DateField(blank=True, null=True, verbose_name="Date to")
+    started_at = models.DateTimeField(blank=True, null=True, verbose_name="Started at")
+    finished_at = models.DateTimeField(
+        blank=True, null=True, verbose_name="Finished at"
+    )
+    calculated = models.BooleanField(default=False, verbose_name="Notebook calculated")
+    success = models.BooleanField(default=False, verbose_name="Execution succeeded")
+    error = models.CharField(
+        max_length=400, blank=True, null=True, verbose_name="Error"
+    )
+    polygon = models.PolygonField(spatial_index=True, verbose_name="Polygon")
+    additional_parameter = models.CharField(
+        max_length=50, null=True, blank=True, verbose_name="Additional parameter"
+    )
 
     @property
     def notebook_name(self):
