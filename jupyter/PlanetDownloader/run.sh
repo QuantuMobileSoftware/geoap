638704d9ddfa9f655452ae06ad56e01ff20113af
Aleksandr Dolgaryev <adolgarev@gmail.com>
2022-11-21 19:35:36 +0200

    portal.soilmate.ai aka 'Ukraine' env (#307)
    * Deploy to instance in EU to serve Planet data
    
    * Run also publish thread
    
    * Revert "Run also publish thread"
    
    This reverts commit 4ec67adc8e82827b91a81df5658192262d163465.
    
    * SSL
    
    * SSL
    
    * SSL
    
    * Set zoom level
    
    * Handle Planet rasters
    
    * Ease zoom level
    
    * Increase tiles generation timeout
    
    * Pass env vars to prod build
    
    * Pass env vars to prod build
    
    * Pass env vars to prod build
    
    * added ability to process skysat (#268)
    
    Co-authored-by: Michael Yushchuk <m.yushchuk@quantumobile.com>
    
    * Ukraine skysat (#269)
    
    * added ability to process skysat
    
    * added visual skysat processing
    
    Co-authored-by: Michael Yushchuk <m.yushchuk@quantumobile.com>
    
    * fix path to visual skysat
    
    * added alpha channel (#271)
    
    Co-authored-by: Michael Yushchuk <m.yushchuk@quantumobile.com>
    
    * Increase maxZoom to 18
    
    * Increase maxZoom to 17
    
    * Increase maxZoom to 17
    
    * Enable background NB execution
    
    * 19080: update path for https://soilmateukraine.quantumobile.co/
    
    * 19080: fix BASE_CONTAINER_NAME
    
    * 19843: fix password for landcover notebook
    
    * Change domain to demo.soilmate.ai
    
    * Change domain to demo.soilmate.ai
    
    * Revert "Change domain to demo.soilmate.ai"
    
    This reverts commit b4ee3d052fa2910e2530d7996d562dc8bcb804ac.
    
    * Change domain to demo.soilmate.ai
    
    * Change domain to portal.soilmate.ai
    
    Co-authored-by: adolgarev <a.dolgarev@quantumobile.com>
    Co-authored-by: Michael Yushchuk <ymv@protonmail.com>
    Co-authored-by: Michael Yushchuk <m.yushchuk@quantumobile.com>
    Co-authored-by: EC2 Default User <ec2-user@ip-172-31-39-219.eu-central-1.compute.internal>

--- jupyter/PlanetDownloader/run.sh
+++ jupyter/PlanetDownloader/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 docker build -f Dockerfile -t planet_downloader_jupyter .
-docker run -d -p 8855:8888 -v /home/quantum/sip:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name planet_downloader_jupyter_1 planet_downloader_jupyter:latest start-notebook.sh --NotebookApp.password="argon2:\$argon2id\$v=19\$m=10240,t=10,p=8\$j8e8HyWWqDT6nIA1F4loUg\$PNWzcQVF1SyKTiuNkZuXYQ"
+docker run -d -p 8855:8888 -v /home/ec2-user/data:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name planet_downloader_jupyter_1 planet_downloader_jupyter:latest start-notebook.sh --NotebookApp.password="argon2:\$argon2id\$v=19\$m=10240,t=10,p=8\$j8e8HyWWqDT6nIA1F4loUg\$PNWzcQVF1SyKTiuNkZuXYQ"
---
+++ webapplication/sip/settings-ukraine.py
@@ -0,0 +1,16 @@
+from sip.settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS.append("soilmateukraine.quantumobile.co")
+ALLOWED_HOSTS.append("demo.soilmate.ai")
+ALLOWED_HOSTS.append("portal.soilmate.ai")
+
+CSRF_TRUSTED_ORIGINS = ["soilmateukraine.quantumobile.co", "demo.soilmate.ai", "portal.soilmate.ai"]
+
+# AOI app, notebook_executor
+NOTEBOOK_EXECUTOR_GPUS = "1"
+BASE_CONTAINER_NAME = "sip_webapplication_1"
+HOST_VOLUME_DATA_BASENAME = "sip"
+
+MAX_TIMEOUT_FOR_TILE_CREATION_SECONDS = 60 * 60 * 1 * 60
