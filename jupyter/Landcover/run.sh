4dec5e35a3f97548fb86644530ca6fa647e1ca99
Artem Sviridov <SvyrydovArtem@gmail.com>
2022-12-14 13:19:43 +0200

    ID21329: Removed landcover

--- jupyter/Landcover/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build -f Dockerfile -t landcover .
-docker run -d -p 8856:8888 -v /home/ec2-user/data:/home/jovyan/work -e JUPYTER_ENABLE_LAB=yes --restart always --name landcover_1 landcover:latest
