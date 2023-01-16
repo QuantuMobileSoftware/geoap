9db9b57f326de801653fab48222af6a0e03d11ae
adolgarev <a.dolgarev@quantumobile.com>
2023-01-16 20:01:40 +0200

    Make one prod env

--- webapplication/sip/settings-prod.py
+++ webapplication/sip/settings-prod.py
@@ -2,15 +2,13 @@ from sip.settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS.append("192.168.1.61")
-ALLOWED_HOSTS.append("soilmate.xyz")
-ALLOWED_HOSTS.append("soilmate.ai")
-ALLOWED_HOSTS.append("demo.soilmate.ai")
-ALLOWED_HOSTS.append("demo2.soilmate.ai")
+ALLOWED_HOSTS.append("portal.soilmate.ai")
 
-CSRF_TRUSTED_ORIGINS = ["soilmate.xyz", "soilmate.ai", "demo.soilmate.ai", "demo2.soilmate.ai"]
+CSRF_TRUSTED_ORIGINS = ["portal.soilmate.ai"]
 
 # AOI app, notebook_executor
 NOTEBOOK_EXECUTOR_GPUS = "1"
-BASE_CONTAINER_NAME = "sip_code_webapplication_1"
+BASE_CONTAINER_NAME = "sip_webapplication_1"
 HOST_VOLUME_DATA_BASENAME = "sip"
+
+MAX_TIMEOUT_FOR_TILE_CREATION_SECONDS = 60 * 60 * 1 * 60
