5e504c47b835df382b74b4b75bde77e5c8fc7148
Dmitri Lukash <43858301+DLukash@users.noreply.github.com>
2023-01-20 19:49:51 +0200

    21185/use Docker container ID as (hostname of container ) as base container Identificator (#325)
    * 21185/use Docker container ID
    (hostname of container ) as base container Identificator
    
    * 21185/made base name contained into a method
    
    * 21185/fixed a typo

--- webapplication/sip/settings-prod.py
+++ webapplication/sip/settings-prod.py
@@ -8,7 +8,6 @@ CSRF_TRUSTED_ORIGINS = ["portal.soilmate.ai"]
 
 # AOI app, notebook_executor
 NOTEBOOK_EXECUTOR_GPUS = "1"
-BASE_CONTAINER_NAME = "sip_webapplication_1"
 HOST_VOLUME_DATA_BASENAME = "sip"
 
 MAX_TIMEOUT_FOR_TILE_CREATION_SECONDS = 60 * 60 * 1 * 60
