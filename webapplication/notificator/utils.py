5713b4e4d41ffe26c8ddb89ed80782f79c722b7a
Dmytro Lukash <dmitri.lukash@gmail.com>
2023-01-24 18:10:36 +0200

    21601/added annotations

--- webapplication/notificator/utils.py
+++ webapplication/notificator/utils.py
@@ -1,11 +1,19 @@
 from notificator.models import Email
 
 
-def send_email_from_template_async(template: str, recipient_email:str, subject:str, context: dict):
-        msg = Email(
-            template=template,
-            recipient_email=recipient_email,
-            subject=subject,
-            context=context
-        )
-        msg.save()
+def send_email_from_template_async(template: str, recipient_email:str, subject:str=None, context: dict=None):
+    """Push an Email to a queue - table in database, associated with Email class
+
+    Args:
+        template (str): name of a Django template
+        recipient_email (str): destination email address 
+        subject (str): subject of the letter
+        context (dict): dictionary of data to fill template and make an email
+    """
+    msg = Email(
+        template=template,
+        recipient_email=recipient_email,
+        subject=subject,
+        context=context
+    )
+    msg.save()
