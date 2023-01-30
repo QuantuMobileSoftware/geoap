b4457111cf7569491b457bf86eab87edd9936c20
Dmytro Lukash <dmitri.lukash@gmail.com>
2023-01-30 10:49:24 +0200

    21601/update user serializer

--- webapplication/user/serializers.py
+++ webapplication/user/serializers.py
@@ -5,10 +5,10 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('pk', 'username', 'email', 'first_name', 'last_name', 'area_limit_ha', 'planet_api_key', 'notify_always', 'email_verified')
-        read_only_fields = ('area_limit_ha', 'email_verified')
+        fields = ('pk', 'username', 'email', 'first_name', 'last_name', 'area_limit_ha', 'planet_api_key', 'notify', 'notification_email')
+        read_only_fields = ('area_limit_ha', 'email')
     
     def validate(self, attrs):
-        if attrs.get('notify_always') and not self.instance.email_verified:
-                raise serializers.ValidationError("Please, verify email first!")
+        if attrs.get('notify') and not (self.instance.notification_email or attrs.get('notification_email')):
+                raise serializers.ValidationError("Please, specify email for notifications first!")
         return super().validate(attrs)
