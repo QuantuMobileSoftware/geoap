c056ed82e5e175a7e3499712f61b3ea8395991ad
Stanislav <35593946+StanislavKharkiv@users.noreply.github.com>
2023-04-11 21:30:15 +0300

    22298/clickable logo (#377)

--- webviewer/src/components/Logo/Logo.jsx
+++ webviewer/src/components/Logo/Logo.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import { ReactComponent as LogoImage } from 'assets/images/logo.svg';
-
+import { ROUTES } from '_constants';
 import { StyledLogo } from './Logo.styles';
 
 export const Logo = ({ ...props }) => {
   return (
-    <StyledLogo {...props}>
-      <LogoImage />
-    </StyledLogo>
+    <Link to={ROUTES.ROOT}>
+      <StyledLogo {...props}>
+        <LogoImage />
+      </StyledLogo>
+    </Link>
   );
 };
