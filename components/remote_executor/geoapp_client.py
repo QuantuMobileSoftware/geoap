9f149837367ba80faf1b5f61905b1faeea68abc9
Ivan Chepets <74767262+Chepa14@users.noreply.github.com>
2023-04-12 17:29:55 +0300

    22194/Implemented script for docker (#372)
    * 22194/Implemented script for docker
    
    * ID22194: Updated tunnel script
    
    * ID22194: added class GeoappClient
    
    * ID22194: renamed to remote_executor
    
    * ID22194: added additional param
    
    * ID22194: Updated script
    
    * ID22194: Added token auth
    
    ---------
    
    Co-authored-by: Artem Sviridov <SvyrydovArtem@gmail.com>

---
+++ components/remote_executor/geoapp_client.py
@@ -0,0 +1,120 @@
+import urllib3
+import json
+import logging
+import time
+import os
+from urllib3.util.retry import Retry
+
+RETRY_INTERVAL = 2880  # num of intervals -> every minute
+RETRY_LIMIT_SECONDS = 172800.0  # 2 days in seconds
+REQUEST_TIMEOUT = 10
+
+
+class GeoappClient:
+    def __init__(self, geoap_creds):
+
+        log = logging.getLogger('urllib3')
+        log.setLevel(logging.DEBUG)
+        log.addHandler(logging.StreamHandler())
+
+        retry_strategy = Retry(total=RETRY_INTERVAL,
+                               backoff_factor=60, status_forcelist=[502, 503, 504])
+        timeout = urllib3.Timeout(
+            total=RETRY_LIMIT_SECONDS, connect=10.0, read=10.0)
+
+        with open(geoap_creds, 'r') as f:
+            geoap_creds_data = json.load(f)
+
+        headers = {
+            'Authorization': f"Token {geoap_creds_data['API_KEY']}",
+            'Accept-Encoding': 'gzip'
+        }
+        http = urllib3.PoolManager(
+            timeout=timeout, retries=retry_strategy, headers=headers)
+
+        self.api_endpoint = geoap_creds_data["API_ENDPOINT"]
+        self.http = http
+        self.log = log
+
+    def get_component_id(self, name):
+        url = self.api_endpoint + "api/notebook"
+        response = self.http.request('GET', url)
+        if response.status != 200:
+            raise Exception(
+                f"Get Component ID error  - status code {response.status}")
+
+        components_list = json.loads(response.data.decode())
+        for component in components_list:
+            if component.get("name", "") == name:
+                component_id = component.get("id")
+                self.log.info(f"component_id: {component_id}")
+                return component_id
+        return None
+
+    def get_user_id(self):
+        url = self.api_endpoint + "api/users/current"
+        response = self.http.request('GET', url)
+        if response.status != 200:
+            raise Exception(
+                f"Get User ID error  - status code {response.status}")
+        user_id = json.loads(response.data.decode()).get("pk")
+        self.log.info(f"user_id: {user_id}")
+        return user_id
+
+    def create_request(self, data):
+        url = self.api_endpoint + "api/request"
+        response = self.http.request('POST', url, fields=data)
+        if response.status not in (200, 201):  # TODO Left only 201
+            raise Exception(
+                f"Create Request error - status code {response.status}")
+        request = json.loads(response.data.decode())
+        self.log.info(f"request: {request}")
+        return request
+
+    def wait_for_request_success_finish(self, request_id):
+        url = self.api_endpoint + f"api/request/{request_id}"
+        start_time = time.time()
+        self.log.info(
+            f"wait_for_request_success started: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))}")
+        while time.time() < start_time + RETRY_LIMIT_SECONDS:
+            response = self.http.request('GET', url)
+            curr_request = json.loads(response.data.decode())
+            if curr_request.get("finished_at"):
+                if curr_request.get("calculated"):
+                    return True, ""
+                else:
+                    return False, curr_request.get("error")
+            else:
+                self.log.info(
+                    f"Not calculated: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))}")
+                time.sleep(60)
+        return False, f"Not finished during {RETRY_LIMIT_SECONDS} seconds"
+
+    def pull_results(self, created_request_id):
+        url = self.api_endpoint + "api/results"
+        paths = []
+        response = self.http.request(
+            'GET', url, fields={"request_id": created_request_id})
+        curr_request = json.loads(response.data.decode())
+        for result in curr_request:
+            paths.append(result.get("filepath"))
+        return paths
+
+    def download_stream_and_save_results(self, result_path, dir_path):
+        url = self.api_endpoint + f"results/{result_path}"
+        self.log.info(f"Started downloading")
+
+        data_path = os.path.join(dir_path, os.path.basename(result_path))
+        self.log.info(f"Data path:{data_path}")
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+            self.log.info(f"Created dir:{data_path}")
+
+        req = self.http.request('GET', url, preload_content=False)
+
+        self.log.info("Started writing result")
+        with open(data_path, 'wb') as f:
+            for chunk in req.stream(1024):
+                f.write(chunk)
+        req.release_conn()
+        self.log.info("Finished writing result, closed connection")
---
+++ components/remote_executor/remote_executor.py
@@ -0,0 +1,39 @@
+import os
+from geoapp_client import GeoappClient
+
+
+if __name__ == "__main__":
+    area = "SRID=4236;"+os.getenv("AOI")
+    request_id = int(os.getenv("REQUEST_ID"))
+    start_date = os.getenv("START_DATE")
+    end_date = os.getenv("END_DATE")
+    output_dir = os.getenv("OUTPUT_FOLDER")
+    component_name = os.getenv("COMPONENT_NAME")
+    geoap_creds = os.getenv("GEOAP_CREDS")
+    additional_parameter_name = os.getenv("ADDITIONAL_PARAMETER_NAME", "")
+    if additional_parameter_name:
+        additional_parameter_value = os.getenv(additional_parameter_name)
+
+    geoapp_client = GeoappClient(geoap_creds)
+    component_id = geoapp_client.get_component_id(component_name)
+    user_id = geoapp_client.get_user_id()
+
+    data = {
+        "user": user_id,
+        "notebook": component_id,
+        "polygon": area,
+        "date_from": start_date if start_date else '',
+        "date_to": end_date if end_date else ''
+    }
+    if additional_parameter_name:
+        data["additional_parameter"] = additional_parameter_value
+
+    request = geoapp_client.create_request(data=data)
+    is_success, error_message = geoapp_client.wait_for_request_success_finish(
+        request.get('id'))
+    if not is_success:
+        raise Exception(error_message)
+    paths_to_results = geoapp_client.pull_results(request.get("id"))
+    for path_to_result in paths_to_results:
+        geoapp_client.download_stream_and_save_results(
+            path_to_result, output_dir)
