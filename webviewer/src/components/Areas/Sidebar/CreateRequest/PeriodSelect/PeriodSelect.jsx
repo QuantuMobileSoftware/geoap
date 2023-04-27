5b27ddcf7a95e22274518e6d9680595836d299dc
Stanislav <35593946+StanislavKharkiv@users.noreply.github.com>
2023-04-27 04:48:18 +0300

    22360/change seasons text (#390)

--- webviewer/src/components/Areas/Sidebar/CreateRequest/PeriodSelect/PeriodSelect.jsx
+++ webviewer/src/components/Areas/Sidebar/CreateRequest/PeriodSelect/PeriodSelect.jsx
@@ -1,15 +1,7 @@
 import React, { useMemo, useState } from 'react';
 import { DATE_TYPES } from '_constants';
+import { SEASONS, SEASON_DATES, WINTER_TEXT, SUMMER_TEXT, getSeasonList } from './utils';
 import { StyledSelect, StyledDayPicker } from './PeriodSelect.styles';
-
-const SEASONS = {
-  summer: 'summer',
-  winter: 'winter'
-};
-const SEASON_DATES = {
-  [SEASONS.summer]: year => ({ start: new Date(year, 3, 1), end: new Date(year, 9, 31) }),
-  [SEASONS.winter]: year => ({ start: new Date(year, 10, 1), end: new Date(year, 2, 31) })
-};
 
 export const PeriodSelect = props => {
   const { notebook, startDate, endDate, setStartDate, setEndDate } = props;
@@ -17,32 +9,10 @@ export const PeriodSelect = props => {
   const [year, setYear] = useState(new Date().getFullYear());
   const [season, setSeason] = useState();
 
-  const startYear = 2015;
-  const minCalendarDate = new Date(startYear, 0, 1);
-  const layerYears = useMemo(
-    () =>
-      Array.from({ length: new Date().getFullYear() - startYear + 1 }).map((el, i) => ({
-        value: startYear + i,
-        name: startYear + i
-      })),
-    []
-  );
-  const SeasonList = useMemo(
-    () =>
-      layerYears.reduce((seasons, item) => {
-        const year = item.value;
-        seasons.push({
-          value: `${SEASONS.summer} ${year}`,
-          name: `Summer ${year}`
-        });
-        seasons.push({
-          value: `${SEASONS.winter} ${year}`,
-          name: `Winter ${year}`
-        });
-        return seasons;
-      }, []),
-    [layerYears]
-  );
+  const START_YEAR = 2015;
+  const MIN_DATE = new Date(START_YEAR, 0, 1);
+
+  const seasonList = useMemo(() => getSeasonList(START_YEAR), []);
 
   const handleYearChange = item => {
     setYear(item.value);
@@ -51,8 +21,8 @@ export const PeriodSelect = props => {
   };
   const handleSeasonChange = data => {
     const [type, year] = data.value.split(' ');
-    setStartDate(SEASON_DATES[SEASONS[type]](year).start);
-    setEndDate(SEASON_DATES[SEASONS[type]](year).end);
+    setStartDate(SEASON_DATES[SEASONS[type]](+year).start);
+    setEndDate(SEASON_DATES[SEASONS[type]](+year).end);
     setSeason(data.value);
   };
   const handleDayChange = date => {
@@ -64,30 +34,30 @@ export const PeriodSelect = props => {
     case DATE_TYPES.year:
       return (
         <StyledSelect
-          items={layerYears}
+          items={START_YEAR}
           onSelect={handleYearChange}
           label='Year'
           value={year}
         />
       );
-    case DATE_TYPES.season:
+    case DATE_TYPES.season: {
+      const year = +season?.split(' ')[1];
       return (
         <>
           <StyledSelect
-            items={SeasonList}
+            items={seasonList}
             label='Season'
             value={season}
             onSelect={handleSeasonChange}
           />
           {season && (
             <p>
-              {season.includes(SEASONS.summer)
-                ? 'Summer season - applies dates from April to November'
-                : 'Winter season - applies dates from November to April'}
+              {season.includes(SEASONS.summer) ? SUMMER_TEXT(year) : WINTER_TEXT(year)}
             </p>
           )}
         </>
       );
+    }
     case DATE_TYPES.day:
       return (
         <StyledDayPicker
@@ -95,7 +65,7 @@ export const PeriodSelect = props => {
           placeholderText='Choose'
           selected={startDate}
           onChange={handleDayChange}
-          minDate={minCalendarDate}
+          minDate={MIN_DATE}
           maxDate={Date.now()}
         />
       );
@@ -107,14 +77,14 @@ export const PeriodSelect = props => {
             placeholderText='From'
             selected={startDate}
             onChange={setStartDate}
-            minDate={minCalendarDate}
+            minDate={MIN_DATE}
             maxDate={Date.now()}
           />
           <StyledDayPicker
             placeholderText='To'
             selected={endDate}
             onChange={setEndDate}
-            minDate={minCalendarDate}
+            minDate={MIN_DATE}
             maxDate={Date.now()}
           />
         </>
