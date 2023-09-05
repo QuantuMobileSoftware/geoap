a9af14d67715b9408d7c931391f214d3b0904250
StanislavKharkiv <s.iosifov@quantumobile.com>
2023-09-05 17:16:37 +0300

    23220/fe/remove_requests_onstart

--- webviewer/src/components/Logo/Logo.jsx
+++ webviewer/src/components/Logo/Logo.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from '_constants';
 import { StyledLogo } from './Logo.styles';
