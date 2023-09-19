fe23b570338770dbe7273596905f3a08120265bc
Artem Sviridov <SvyrydovArtem@gmail.com>
2023-09-19 15:31:20 +0300

    ID23420:removed unused field

--- webapplication/aoi/admin.py
+++ webapplication/aoi/admin.py
@@ -18,7 +18,6 @@ class ComponentAdmin(admin.OSMGeoAdmin):
                     'additional_parameter', 'period_required',)
     search_fields = ('name', 'image', 'path', 'kernel_name', 'run_validation', 'success', )
     readonly_fields = ('pk',)
-    exclude = ('sentinel_image_type',)
 
     form = ComponentAdminForm
 
