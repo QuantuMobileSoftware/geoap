0665e152dfbc7eeeaf826ce99aeac9716e1f2886
StanislavKharkiv <s.iosifov@quantumobile.com>
2023-10-24 17:41:33 +0300

    23626/change transactions page

--- webviewer/src/api/areas/areas.endpoints.js
+++ webviewer/src/api/areas/areas.endpoints.js
@@ -3,6 +3,7 @@ const byId = id => `${root}/${id}`;
 const requests = id => `${byId(id)}/requests`;
 const results = id => `${byId(id)}/results`;
 const result = id => `results/${id}`;
+const allResults = '/results';
 const field = id => `results/${id}/field`;
 const request = '/request';
 const layers = '/notebook';
@@ -13,6 +14,7 @@ export const areasEndpoints = {
   requests,
   results,
   result,
+  allResults,
   layers,
   request,
   field
---
+++ webviewer/src/pages/Account/components/Transactions/InfoModal/InfoModal.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { em } from 'styles';
+
+export const Content = styled.div`
+  margin: ${em(20)} 0;
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
