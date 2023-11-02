83c13b509a287f556648efde31d3f4544e2c6328
Seneckiy <y.senetskiy@quantumobile.com>
2023-11-02 16:38:50 +0200

    add geoap_email_notifications

--- webapplication/aoi/management/commands/_notebook.py
+++ webapplication/aoi/management/commands/_notebook.py
@@ -24,6 +24,19 @@ logger = logging.getLogger(__name__)
 
 THREAD_SLEEP = 10
 
+
+def email_notification(request, aoi, status):
+    try:
+        from geoap_email_notifications.utils import email_notification
+        email_notification(request, status)
+    except ModuleNotFoundError:
+        # Error handling
+        logger.info(
+            f"""Your request for AOI '{aoi.name if aoi else request.polygon.wkt}' 
+            and layer '{request.component_name}' is succeeded"""
+        )
+
+
 def clean_container_logs(logs):
     # Remove line numbers
     # like from this "00m [38;5;167;01mValueError[39;00m([38;5;124m"[39m[38;5;124mImages not loaded for given AOI."
@@ -38,49 +51,6 @@ def clean_container_logs(logs):
     # Replace multiple spaces
     log_text = re.sub(r'\s+', ' ', log_text)
     return log_text
-
-def send_email_notification(user_mail, email_message, subject):
-    result = 0
-    try:
-        result = send_mail(subject, email_message, None, [user_mail])
-    except Exception as ex:
-        logger.error(f"Error while sending mail: {str(ex)}")
-    if result == 1:
-        logger.info(f"Email sent successfully! for email '{user_mail}'")
-    else:
-        logger.info(f"Failed to send the email for email '{user_mail}'")
-
-def email_notification(request, status):
-    user_data = User.objects.filter(id=request.user_id).first()
-    aoi_name = AoI.objects.filter(id=request.aoi_id).first()
-    if not user_data.receive_notification:
-        logger.info(f"Not sending email for user '{user_data.email}'")
-        return
-
-    message = f"""Your request for AOI '{aoi_name.name if aoi_name else request.polygon.wkt}' and layer '{request.component_name}' is {status}
-    \n\nClick the link below to visit the site:\n{request.request_origin}"""
-    send_email_notification(user_data.email, message, settings.EMAIL_SUBJECT)
-
-    if settings.DEFAULT_SYSTEM_NOTIFICATION_EMAIL:
-        system_message=f"""
-        Status: {status.upper()},
-        Error: {', '.join(request.user_readable_errors) if request.user_readable_errors else request.error},
-        Domain: {request.request_origin},
-        
-        AoI Name: {aoi_name.name if aoi_name else None},
-        AoI polygon: {request.polygon.wkt},
-        Component name: {request.component_name},
-        Start date: {request.date_from.strftime("%Y/%m/%d") if request.date_from else None},
-        End date: {request.date_to.strftime("%Y/%m/%d") if request.date_to else None},
-        Additional parameter value: {request.additional_parameter},
-        
-        User name: {user_data.username},
-        User email: {user_data.email}
-        """
-        send_email_notification(settings.DEFAULT_SYSTEM_NOTIFICATION_EMAIL, system_message, f"{settings.EMAIL_SUBJECT} - {status.upper()}")
-
-
-
 
 
 class StoppableThread(ABC, Thread):
@@ -171,6 +141,7 @@ class NotebookDockerThread(StoppableThread):
         for container in exited_containers:
             attrs = Container.container_attrs(container)
             request = Request.objects.get(pk=attrs['pk'])
+            aoi = AoI.objects.filter(id=request.aoi_id).first()
             if attrs['exit_code'] == 0:
                 logger.info(f"Notebook in container {container.name} executed successfully")
                 request.calculated = True
@@ -207,8 +178,7 @@ class NotebookDockerThread(StoppableThread):
                 with transaction.atomic():
                     request_transaction.save(update_fields=("rolled_back", "completed", "error"))
                     request_transaction.user.save(update_fields=("on_hold",))
-
-                email_notification(request, "failed")
+                email_notification(request, aoi, "failed")
             try:
                 container.remove()
             except:
@@ -241,8 +211,8 @@ class NotebookDockerThread(StoppableThread):
                         request_transaction.save(update_fields=("rolled_back",))
                         request_transaction.user.save(update_fields=("on_hold",))
                         request.save(update_fields=['finished_at'])
-
-                        email_notification(request, "failed")
+                        aoi = AoI.objects.filter(id=request.aoi_id).first()
+                        email_notification(request, aoi, "failed")
                 except Exception as ex:
                     logger.error(f"Cannot update request {request.pk} in db: {str(ex)}")
 
@@ -273,7 +243,9 @@ class PublisherThread(StoppableThread):
                     request_transaction.save(update_fields=("completed",))
                     request_transaction.user.save(update_fields=("balance", "on_hold"))
 
-                email_notification(sr, "succeeded")
+                aoi = AoI.objects.filter(id=sr.aoi_id).first()
+                email_notification(sr, aoi,  "succeeded")
+
         success_requests.update(finished_at=localtime(), success=True)
 
 
