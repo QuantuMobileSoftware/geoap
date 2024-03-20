880d5011c7e5e3a11876c6553f2b2a981ef1bcb3
StanislavKharkiv <shtas89@gmail.com>
2024-03-20 16:24:52 +0100

    23023/add loader indicator in download results

--- webviewer/src/api/files/files.requests.js
+++ webviewer/src/api/files/files.requests.js
@@ -1,9 +1,13 @@
 import { axiosInstance } from 'api/instance';
 import { filesEndpoints } from './files.endpoints';
 
-const getFile = async path => {
+const getFile = async (path, onProgress) => {
   const resp = await axiosInstance.get(`${filesEndpoints.results}/${path}`, {
-    responseType: 'blob'
+    responseType: 'blob',
+    onDownloadProgress: progressEvent => {
+      const percentCompleted = (progressEvent.loaded / progressEvent.total) * 100;
+      onProgress({ percents: Math.round(percentCompleted), size: progressEvent.total });
+    }
   });
   return resp.data;
 };
--- webviewer/src/components/FileLoader/FileLoader.styles.js
+++ webviewer/src/components/FileLoader/FileLoader.styles.js
@@ -6,7 +6,7 @@ import { Button } from 'components/_shared/Button';
 export const StyledFileLoader = styled.div`
   ${({ theme, top }) => css`
     position: fixed;
-    top: ${em(top ? 80 : 290)};
+    top: ${em(top ? 80 : 360)};
     right: ${em(20)};
     width: ${em(170)};
     display: flex;
@@ -24,6 +24,8 @@ export const FileLoaderContent = styled.div`
     padding: ${em(10)};
     background: ${theme.colors.nature.n0};
     border-radius: 5px;
+    max-height: ${em(300)};
+    overflow-y: auto;
   `}
 `;
 
@@ -45,4 +47,34 @@ export const DownloadButton = styled(Button)`
   margin-top: ${em(10)};
   padding: 0;
   text-decoration: underline;
+`;
+
+export const ProgressName = styled.div`
+  ${({ theme }) => css`
+    margin-top: ${em(10)};
+    font-size: ${em(10)};
+    font-weight: ${theme.fontWeights[1]};
+  `}
+`;
+
+export const ProgressFileInfo = styled.div`
+  margin: ${em(5)} 0;
+  font-size: ${em(12)};
+`;
+
+export const ProgressBar = styled.div`
+  ${({ theme, percents }) => css`
+    background: ${theme.colors.primary.p4};
+    height: 4px;
+    width: 100%;
+    border-radius: 5px;
+    overflow: hidden;
+    &::after {
+      content: '';
+      display: block;
+      height: 100%;
+      width: ${percents}%;
+      background: ${theme.colors.primary.p3};
+    }
+  `}
 `;
