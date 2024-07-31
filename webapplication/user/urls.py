43a7e521e42968ccf0cf4d30c240c06ac77e3703
Artem Sviridov <SvyrydovArtem@gmail.com>
2024-07-31 16:45:22 +0300

    ID25588: Test Google bucket flow (for request)

--- webapplication/user/urls.py
+++ webapplication/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views import TransactionListAPIView
+from user.views import TransactionListAPIView, GoogleBucketFolderAPIView
 
 urlpatterns = [
     path('transactions/', TransactionListAPIView.as_view(), name='get_transactions_list'),
+    path('google_bucket_folder/', GoogleBucketFolderAPIView.as_view(), name='get_transactions_list'),
 ]
