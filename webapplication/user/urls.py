3d24b0a41d02fb3d479608bf2dc31f9eb7c7b105
Artem Sviridov <SvyrydovArtem@gmail.com>
2024-08-06 22:03:55 +0300

    ID25625: test fix

--- webapplication/user/urls.py
+++ webapplication/user/urls.py
@@ -4,5 +4,5 @@ from user.views import TransactionListAPIView, GoogleBucketFolderAPIView
 
 urlpatterns = [
     path('transactions/', TransactionListAPIView.as_view(), name='get_transactions_list'),
-    path('google_bucket_folder/', GoogleBucketFolderAPIView.as_view(), name='get_transactions_list'),
+    path('google_bucket_folder/', GoogleBucketFolderAPIView.as_view(), name='google_bucket_folder'),
 ]
