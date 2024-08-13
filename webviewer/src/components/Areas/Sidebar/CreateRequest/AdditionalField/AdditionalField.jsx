6560e4ca91187f6a9337c4ef556ef21f2255c230
VadymPertsov <v.pertsov@quantumobile.com>
2024-08-13 12:32:52 +0300

    25681: add isHidden prop to AdditionalField

--- webviewer/src/components/Areas/Sidebar/CreateRequest/AdditionalField/AdditionalField.jsx
+++ webviewer/src/components/Areas/Sidebar/CreateRequest/AdditionalField/AdditionalField.jsx
@@ -3,10 +3,10 @@ import { StyledInput, InputWrapper } from './AdditionalField.styles';
 import { Label } from 'components/_shared/Select';
 import { isNil } from 'lodash-es';
 
-export const AdditionalField = ({ label, value, onChange }) => {
+export const AdditionalField = ({ label, value, onChange, isHidden = false }) => {
   if (isNil(label)) return null;
   return (
-    <InputWrapper>
+    <InputWrapper isHidden={isHidden}>
       <Label>{label}</Label>
       <StyledInput value={value} onChange={onChange} />
     </InputWrapper>
