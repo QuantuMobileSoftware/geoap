e4a165b457a9d677e87163307095e3298b0e4eaf
VadymPertsov <v.pertsov@quantumobile.com>
2024-08-29 17:52:51 +0300

    25804: add Done text to LoadProgress

--- webviewer/src/components/FileLoader/LoadProgress.jsx
+++ webviewer/src/components/FileLoader/LoadProgress.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { ProgressName, ProgressBar, ProgressFileInfo } from './FileLoader.styles';
+import {
+  ProgressName,
+  ProgressBar,
+  ProgressFileInfo,
+  ProgressWrapper
+} from './FileLoader.styles';
+import { Done } from 'assets/icons';
 
 const ONE_MB = 1000000;
 const ONE_KB = 1000;
@@ -10,11 +16,34 @@ export const LoadProgress = ({ progress }) => {
   const fileSize =
     size > ONE_MB ? `${(size / ONE_MB).toFixed(1)}mb` : `${(size / ONE_KB).toFixed(1)}kb`;
 
+  const [showDone, setShowDone] = useState(false);
+
+  useEffect(() => {
+    if (percents === 100) {
+      const timer = setTimeout(() => {
+        setShowDone(true);
+      }, 3000);
+
+      return () => clearTimeout(timer);
+    } else {
+      setShowDone(false);
+    }
+  }, [percents]);
+
   return (
-    <div>
+    <ProgressWrapper>
       <ProgressName>{name}</ProgressName>
-      <ProgressFileInfo>{fileSize} - Loading</ProgressFileInfo>
-      <ProgressBar percents={percents} />
-    </div>
+      <ProgressFileInfo>
+        <span>{fileSize} - </span>
+        {percents !== 100 || !showDone ? (
+          <span>Loading</span>
+        ) : (
+          <span>
+            Done <Done />
+          </span>
+        )}
+      </ProgressFileInfo>
+      {(percents !== 100 || !showDone) && <ProgressBar percents={percents} />}
+    </ProgressWrapper>
   );
 };
