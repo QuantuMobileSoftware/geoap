e9211f32efea62ee29e82665b0923687618802a4
Artem Sviridov <SvyrydovArtem@gmail.com>
2025-02-21 14:19:12 +0200

    ID26981: Added filter for stones

--- webviewer/src/components/Areas/Sidebar/CreateRequest/StoneOptions/StoneOptions.styles.js
+++ webviewer/src/components/Areas/Sidebar/CreateRequest/StoneOptions/StoneOptions.styles.js
@@ -11,6 +11,25 @@ export const Label = styled(Typography).attrs({ variant: 'caption1' })`
   `}
 `;
 
+export const LabelWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${rem(8)};
+  margin-bottom: ${em(20)};
+`;
+
+export const StyledSelectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: ${em(8)};
+
+  input {
+    padding: ${em(8)};
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+`;
+
 export const StyledSelect = styled(Select)`
   margin-bottom: ${em(20)};
   ${Option} {
@@ -20,9 +39,44 @@ export const StyledSelect = styled(Select)`
   }
 `;
 
-export const LabelWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: ${rem(8)};
-  margin-bottom: ${em(20)};
+export const DropdownContainer = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+export const StyledInput = styled.input`
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  color: #898989;
+  &:focus-visible {
+    outline-color: #efefef;
+    outline-width: 0;
+  }
+`;
+
+export const DropdownList = styled.ul`
+  position: absolute;
+  width: 100%;
+  background: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  list-style: none;
+  margin-top: 4px;
+  padding: 0;
+  max-height: 200px;
+  overflow-y: auto;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+  z-index: 1000; /* Increase the z-index */
+`;
+
+export const DropdownItem = styled.li`
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    color: #ffffff;
+    background: #35603d;
+  }
 `;
