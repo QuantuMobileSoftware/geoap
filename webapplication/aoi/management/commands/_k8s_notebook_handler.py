2a6c0f23e74e77860dabb35d1a7ddb4f1200d588
Artem Sviridov <SvyrydovArtem@gmail.com>
2025-03-18 19:09:27 +0200

    ID27113: error log

--- webapplication/aoi/management/commands/_k8s_notebook_handler.py
+++ webapplication/aoi/management/commands/_k8s_notebook_handler.py
@@ -10,7 +10,6 @@ from kubernetes.client.rest import ApiException
 from aoi.models import Component, Request
 from aoi.management.commands.executor import NotebookExecutor
 from aoi.management.commands._ComponentExecutionHelper import ComponentExecutionHelper
-from aoi.management.commands._notebook import clean_container_logs
 
 from django.conf import settings
 from django.utils.timezone import localtime
