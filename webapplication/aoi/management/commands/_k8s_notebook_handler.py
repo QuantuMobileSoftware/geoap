330ff1402d5f3d2c4a367a81171a0b06978f6232
Artem Sviridov <SvyrydovArtem@gmail.com>
2025-03-27 00:19:10 +0200

    ID27113: memory limits

--- webapplication/aoi/management/commands/_k8s_notebook_handler.py
+++ webapplication/aoi/management/commands/_k8s_notebook_handler.py
@@ -145,8 +145,8 @@ class K8sNotebookHandler(ComponentExecutionHelper):
                 "nvidia.com/gpu": str(settings.GPU_CORES_PER_NOTEBOOK)
             },
             limits = {
-                "cpu": "4000m",
-                "memory": "15Gi",
+                "cpu": "3500m",
+                "memory": "14Gi",
                 "nvidia.com/gpu": str(settings.GPU_CORES_PER_NOTEBOOK)
             }
         )
