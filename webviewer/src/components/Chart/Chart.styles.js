43b94e376d821af5c10ed08feea4fdcb017e19c1
Artem Sviridov <SvyrydovArtem@gmail.com>
2025-05-21 14:32:17 +0300

    ID27113: added chart fix

--- webviewer/src/components/Chart/Chart.styles.js
+++ webviewer/src/components/Chart/Chart.styles.js
@@ -6,3 +6,19 @@ export const Wrapper = styled.div`
   right: 15px;
   z-index: ${({ theme }) => theme.zIndexes[2]};
 `;
+export const CloseButton = styled.button`
+  position: absolute;
+  top: 4px;
+  right: 6px;
+  background: transparent;
+  border: none;
+  font-size: 20px;
+  font-weight: bold;
+  cursor: pointer;
+  color: #333;
+  z-index: 1;
+
+  &:hover {
+    color: #e00;
+  }
+`;
